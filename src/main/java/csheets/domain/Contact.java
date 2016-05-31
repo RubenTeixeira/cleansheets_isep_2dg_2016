39ca44bdd0edae466e33902b05d74646a8371df0
1130303 <1130303@isep.ipp.pt>
2016-05-31 19:08:24 +0100

    LPFOURDG-100
    Ammend last commit.

--- src/main/java/csheets/domain/Contact.java
+++ src/main/java/csheets/domain/Contact.java
@@ -5,20 +5,40 @@
  */
 package csheets.domain;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  *
  * @author nervousDev
  */
-public class Contact {
+@Entity
+public class Contact implements Serializable {
 
-	private String firstName;
-	private String lastName;
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private String firstName;
+    private String lastName;
 
-	public Contact(String firstName, String lastName) {
+    protected Contact() {
+    }
 
-		this.firstName = firstName;
-		this.lastName = lastName;
-		throw new IllegalArgumentException();
-	}
+    public Contact(String firstName, String lastName) {
+        if(firstName == null || lastName == null)
+        {
+            throw new IllegalArgumentException();
+            
+        } else if(firstName.isEmpty() || lastName.isEmpty())
+        {
+            throw new IllegalArgumentException();
+        }
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
 }
