de7585f726fba1a7909a1c5db109f4880f12a6eb
1130383 <1130383@isep.ipp.pt>
2016-05-31 17:51:19 +0100

    LPFOURDG-102 #Comment Analysis, Classes regarding the specific Task #time 1h

---
+++ src/main/java/csheets/core/formula/OperationsBlock.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csheets.core.formula;
+
+/**
+ * A Set of Operations.
+ *
+ * @author Pedro Gomes 1130383@isep.ipp.pt
+ */
+public class OperationsBlock {
+
+}
---
+++ src/main/java/csheets/core/formula/lang/For.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csheets.core.formula.lang;
+
+/**
+ * Execution of For Loop.
+ *
+ * @author Pedro Gomes 1130383@isep.ipp.pt
+ */
+public class For {
+
+}
