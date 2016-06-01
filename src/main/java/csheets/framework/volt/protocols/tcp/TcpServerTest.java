d5912cd4b5153e113a45c1dd4250fa520e7387fc
Carlos Mateus <1130105@isep.ipp.pt>
2016-06-01 17:10:05 +0100

    LPFOURDG-106 fix tests

--- src/main/java/csheets/framework/volt/protocols/tcp/TcpServerTest.java
+++ src/main/java/csheets/framework/volt/protocols/tcp/TcpServerTest.java
@@ -13,20 +13,28 @@ import java.util.Map;
  * @author oxy
  */
 public class TcpServerTest {
-    
-    public static void main(String[] args) 
-    {
-        TcpServer server = new TcpServer();
-        
-        server.expect(":hi", new Action() {
-            public void run(Map<String, Object> args)
-            {
-                System.out.println(args.get("message"));
-                
-                server.send(":hi", (String) args.get("from"), "Hi back m8");
-            }
-        });
-        
-        server.stream(8000);
-    }
+
+	public static void main(String[] args) {
+		TcpServer server = new TcpServer();
+
+		server.expect(":hi", new Action() {
+					  public void run(Map<String, Object> args) {
+						  System.out.println(args.get("message"));
+
+						  server.
+							  send(":hi", (String) args.get("from"), "Hi back m8");
+					  }
+				  });
+
+		server.expect(":olaaa", new Action() {
+					  public void run(Map<String, Object> args) {
+						  System.out.println(args.get("message"));
+
+						  server.
+							  send(":olaaa", (String) args.get("from"), "recebi");
+					  }
+				  });
+
+		server.stream(8000);
+	}
 }
