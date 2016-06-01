b5218f6a3f6b1d14372b79afb4f69b1acd0d2238
1130303 <1130303@isep.ipp.pt>
2016-06-02 00:29:45 +0100

    LPFOURDG-75
    Implementation still in progress.

--- src/main/java/csheets/domain/Event.java
+++ src/main/java/csheets/domain/Event.java
@@ -12,13 +12,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
+import javax.persistence.UniqueConstraint;
 
 /**
  *
  * @author Rui Freitas
  */
 @Entity
+@Table(uniqueConstraints = {
+	@UniqueConstraint(columnNames = {"DATE", "DESCRIPTION"})})
 public class Event implements Serializable {
 
 	@Id
