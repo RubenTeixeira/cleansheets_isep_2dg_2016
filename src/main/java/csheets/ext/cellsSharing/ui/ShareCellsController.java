4a6d02ee3a78e824b406310bb4f9669956aeb99c
Rafael Rocha <1140329@isep.ipp.pt>
2016-06-01 16:24:45 +0100

    #LPFOURDG-51 sub task : LPFOURDG-107 #time 15m #comment Updated ShareCellsController.

--- src/main/java/csheets/ext/cellsSharing/ui/ShareCellsController.java
+++ src/main/java/csheets/ext/cellsSharing/ui/ShareCellsController.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package csheets.ext.cellsSharing.ui;
 
 import csheets.ui.ctrl.UIController;
-import java.util.List;
 
 public class ShareCellsController {
 
@@ -19,60 +13,17 @@ public class ShareCellsController {
      * User interface panel *
      */
     private final SharePanel uiPanel;
-    
-    private UdpServerService updServerService;
 
     public ShareCellsController(UIController uiController, SharePanel uiPanel, int port) {
 
         this.uiController = uiController;
         this.uiPanel = uiPanel;
-
-        Thread udpServer = new Thread() {
-            @Override
-            public void run() {
-                new UdpClientService();
-            }
-        };
-
-        Thread udpClient = new Thread() {
-            @Override
-            public void run() {
-                updServerService = new UdpServerService();
-            }
-        };
-        Thread tcpServer = new Thread() {
-            public void run() {
-                new TcpServerService();
-            }
-        };
-
-        Thread tcpClient = new Thread() {
-            public void run() {
-                new TcpClientService(port);
-            }
-        };
-
-        udpServer.start();
-        udpClient.start();
-        tcpServer.start();
-        tcpClient.start();
-    }
-
-    /**
-     *
-     * @param observer
-     * @param observable
-     */
-    public void addObserverToServer(SharePanel observer) {
-        this.updServerService.addObserver(observer);
-    }
-
-    /**
-     * Sends a broadcast signal requesting for hosts (their ip and port).
-     *
-     * @return List of available hosts
-     */
-    public List<String> getOtherInstances() {
-        return null;
+        
+        UdpClientService udpClientService = new UdpClientService();
+        UdpServerService udpServerService = new UdpServerService();
+        TcpServerService tcpServerService = new TcpServerService();
+        
+        udpServerService.addObserver(uiPanel);
+        //tcpServerService.addObserver(uiPanel);
     }
 }
