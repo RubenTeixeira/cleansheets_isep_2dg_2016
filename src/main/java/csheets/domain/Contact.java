89ba32f78ad6981492f4f904cf3d1e91b00b4338
1130303 <1130303@isep.ipp.pt>
2016-06-01 14:41:59 +0100

    LPFOURDG-75 #time 2h
    Small changes. Implementation still in progress.

--- src/main/java/csheets/domain/Contact.java
+++ src/main/java/csheets/domain/Contact.java
@@ -9,41 +9,61 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 /**
  * This class is an AggregateRoot. Represents an domain entity Contact.
- *
  * @author Rui Freitas
  */
 @Entity
 public class Contact implements Serializable {
 
-	@Id
-	@GeneratedValue
-	private Long id;
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private Agenda agenda;
+    
+    private String firstName;
+    private String lastName;
+    
+    @Lob
+    private byte[] photo;
 
-	private Agenda agenda;
+    protected Contact() {
+    }
 
-	private String firstName;
-	private String lastName;
+    public Contact(String firstName, String lastName) {
+        if(firstName == null || lastName == null)
+        {
+            throw new IllegalArgumentException();
+            
+        } else if(firstName.isEmpty() || lastName.isEmpty())
+        {
+            throw new IllegalArgumentException();
+        }
 
-	protected Contact() {
-	}
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    public String firstName()
+    {
+        return this.firstName;
+    }
 
-	public Contact(String firstName, String lastName) {
-		if (firstName == null || lastName == null) {
-			throw new IllegalArgumentException();
-
-		} else if (firstName.isEmpty() || lastName.isEmpty()) {
-			throw new IllegalArgumentException();
-		}
-
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-
-	@Override
-	public String toString() {
-		return this.firstName + " - " + this.lastName;
-	}
+    public String lastName()
+    {
+        return this.lastName;
+    }
+    
+    public byte[] photo()
+    {
+        return this.photo;
+    }
+    
+    public void changePhoto(byte[] photo)
+    {
+        this.photo = photo;
+    }
 }
