b6b0291a64238c19c5ea34b01552932fda88100e
Rui Bento <1141071@isep.ipp.pt>
2016-06-02 01:22:41 +0100

    LPFOURDG-112 #time 9h #comment Improved Extenstions activate and deactivate UI.

--- src/main/java/csheets/ui/ExtensionsActivDeactivUI.java
+++ src/main/java/csheets/ui/ExtensionsActivDeactivUI.java
@@ -16,110 +16,147 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author yur
  */
-public class ExtenstionsActDeactUI extends JFrame {
-    
-    public static final String TITLE = "NAME";
-    
+public class ExtensionsActivDeactivUI extends JFrame {
+
+    public static final String TITLE = "Activate/Deactivate Extensions";
+
+    private final Object[] tablesNames = {"Extension Name", "Enable"};
+
     private UIController controller;
-    private Panel panel;
+    private JLabel lblTitle;
+    private JScrollPane scrollPane;
     private JTable tableExtensions;
     private DefaultTableModel modelTableExtensions;
+    private Panel panel;
     private JButton btnApply, btnCancel;
-    private final Object[] tablesNames = {"Extension Name", "Enable"};
-    
-    public ExtenstionsActDeactUI(UIController controller) {
+
+    public ExtensionsActivDeactivUI(UIController controller) {
         this.controller = controller;
         createUIObjects();
         configureUIObjects();
         positionUIObjects();
         fetchExtensions();
     }
-    
+
     private void createUIObjects() {
-        this.panel = new Panel();
         this.modelTableExtensions = new DefaultTableModel(0, tablesNames.length) {
             public Class<?> getColumnClass(int column) {
-                switch(column) {
-                    case 0: return String.class;
-                    case 1: return Boolean.class;
-                    default: return String.class;
+                switch (column) {
+                    case 0:
+                        return String.class;
+                    case 1:
+                        return Boolean.class;
+                    default:
+                        return String.class;
                 }
             }
         };
-        this.tableExtensions = new JTable(this.modelTableExtensions);
+        this.lblTitle = new JLabel();
+        this.tableExtensions = new JTable(this.modelTableExtensions) {
+            public boolean isCellEditable(int row, int column) {
+                if (column==1) return true;
+                return false;
+            };
+        };
+        this.scrollPane = new JScrollPane(tableExtensions);
+        this.panel = new Panel();
         this.btnApply = new JButton();
         this.btnCancel = new JButton();
     }
-    
+
     private void positionUIObjects() {
         Container pane = getContentPane();
-        pane.add(tableExtensions, BorderLayout.CENTER);
+        pane.add(lblTitle, BorderLayout.NORTH);
+        pane.add(scrollPane, BorderLayout.CENTER);
         pane.add(panel, BorderLayout.SOUTH);
+        pack();
     }
-    
+
     private void configureUIObjects() {
         confFrame();
+        confLblTitle();
+        confScrollPane();
         confTable();
         confPanel();
         confBtnApply();
         confBtnCancel();
     }
-    
+
     private void confFrame() {
-        Dimension defaultSize = new Dimension(400, 420);
+        Dimension defaultSize = new Dimension(350, 420);
+        setAlwaysOnTop(true);
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setEnabled(true);
         setPreferredSize(defaultSize);
+        setResizable(false);
         setSize(defaultSize);
         setTitle(TITLE);
         setVisible(true);
     }
-    
+
+    private void confLblTitle() {
+        lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        lblTitle.setText(TITLE);
+    }
+
+    private void confScrollPane() {
+    }
+
     private void confTable() {
-        tableExtensions.setEditingColumn(1);
+        modelTableExtensions.setColumnIdentifiers(tablesNames);
+        tableExtensions.setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
+        tableExtensions.setDragEnabled(false);
     }
-    
+
     private void confPanel() {
-        this.panel.add(btnApply);
-        this.panel.add(btnCancel);
+        panel.add(btnApply);
+        panel.add(btnCancel);
     }
-    
+
     private void confBtnApply() {
         btnApply.setText("Apply");
         btnApply.addActionListener(new ActionListener() {
-            
+
             public void actionPerformed(ActionEvent e) {
-                for (int i = 0; i < modelTableExtensions.getRowCount(); i++) {
-                    Object extensionName = modelTableExtensions.getValueAt(i, 0);
-                    Object extensionEnable = modelTableExtensions.getValueAt(i, 1);
-                    controller.changeExtensionState((String)extensionName, (boolean)extensionEnable);
-                }
-                
+                btnApplyAction();
             }
         });
     }
-    
+
     private void confBtnCancel() {
         btnCancel.setText("Cancel");
         btnCancel.addActionListener(e -> this.dispose());
     }
-    
+
     private void fetchExtensions() {
         for (UIExtension uIExtension : controller.getExtensions()) {
             addExtensionToModel(uIExtension.getExtension());
         }
     }
-    
+
     private void addExtensionToModel(Extension extension) {
         if (extension != null) {
             modelTableExtensions.addRow(new Object[]{extension.getName(), extension.isEnabled()});
         }
+    }
+
+    private void btnApplyAction() {
+        for (int i = 0; i < modelTableExtensions.getRowCount(); i++) {
+            Object extensionName = modelTableExtensions.getValueAt(i, 0);
+            Object extensionEnable = modelTableExtensions.getValueAt(i, 1);
+            controller.changeExtensionState((String) extensionName, (boolean) extensionEnable);
+        }
+        this.dispose();
     }
 }
