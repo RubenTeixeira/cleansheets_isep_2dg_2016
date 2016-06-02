e4145bf36be93daafc9e370f2f84429b4b2265c4
1140780@isep.ipp.pt <1140780@isep.ipp.pt>
2016-06-02 09:53:13 +0100

    LPFOURDG-101 #time 5m Wrong exception type was being thrown on For: if condition evaluation result is not a boolean, then the exception we throw is an IllegalValueTypeException

--- src/main/java/csheets/core/formula/lang/For.java
+++ src/main/java/csheets/core/formula/lang/For.java
@@ -14,82 +14,82 @@ import csheets.core.formula.InstructionBlock;
  */
 public class For implements Function {
 
-    //private FunctionParameter[] args;
-    private Expression[] args;
+	//private FunctionParameter[] args;
+	private Expression[] args;
 
 //	public For(Expression[] args){
 //		//explode
 //		this.args = (FunctionParameter[]) args;
 //	}
-    /**
-     * Create For.
-     *
-     * @param args Variable number of expression arguments.
-     */
-    public For(Expression... args) {
-        this.args = args;
+	/**
+	 * Create For.
+	 *
+	 * @param args Variable number of expression arguments.
+	 */
+	public For(Expression... args) {
+		this.args = args;
 
-    }
+	}
 
-    /**
-     *
-     * @return Identifier
-     */
-    @Override
-    public String getIdentifier() {
-        return "For";
-    }
+	/**
+	 *
+	 * @return Identifier
+	 */
+	@Override
+	public String getIdentifier() {
+		return "For";
+	}
 
-    /**
-     *
-     * @param args Array of expressions.
-     * @return Returns the value of a cell.
-     * @throws IllegalValueTypeException The value can be illegal.
-     */
-    @Override
-    public Value applyTo(Expression[] args) throws IllegalValueTypeException {
-        Expression assignment = args[0];
-        Expression limiter = args[1];
-        //expressions[] for instructionblock.
-        Expression[] expBlock = new Expression[args.length - 2];
-        int j = 0;
-        for (int i = 2; i < args.length; i++) {
-            expBlock[j] = args[i];
-            j++;
-        }
-        InstructionBlock block = new InstructionBlock(expBlock);
-        //starting point.
-        assignment.evaluate();
-        //limiter validation
-        Value flag = limiter.evaluate();
-        if (flag.toBoolean() == false) {
-            throw new IllegalThreadStateException();
-        }
-        Value value = new Value();
-        while (args[1].evaluate().toBoolean()) {
-            value = block.evaluate();
-        }
-        return value;
-    }
+	/**
+	 *
+	 * @param args Array of expressions.
+	 * @return Returns the value of a cell.
+	 * @throws IllegalValueTypeException The value can be illegal.
+	 */
+	@Override
+	public Value applyTo(Expression[] args) throws IllegalValueTypeException {
+		Expression assignment = args[0];
+		Expression limiter = args[1];
+		//expressions[] for instructionblock.
+		Expression[] expBlock = new Expression[args.length - 2];
+		int j = 0;
+		for (int i = 2; i < args.length; i++) {
+			expBlock[j] = args[i];
+			j++;
+		}
+		InstructionBlock block = new InstructionBlock(expBlock);
+		//starting point.
+		assignment.evaluate();
+		//limiter validation
+		Value flag = limiter.evaluate();
+		if (flag.toBoolean() == false) {
+			throw new IllegalValueTypeException(flag, flag.getType());
+		}
+		Value value = new Value();
+		while (args[1].evaluate().toBoolean()) {
+			value = block.evaluate();
+		}
+		return value;
+	}
 
-    /**
-     * TODO.
-     *
-     * @return Gets the function parameters.
-     */
-    @Override
-    public FunctionParameter[] getParameters() {
-        return null;
-    }
+	/**
+	 * TODO.
+	 *
+	 * @return Gets the function parameters.
+	 */
+	@Override
+	public FunctionParameter[] getParameters() {
+		return null;
+	}
 
-    /**
-     * n Arguments.
-     *
-     * @return true
-     */
-    @Override
-    public boolean isVarArg() {
-        return true;
+	/**
+	 * n Arguments.
+	 *
+	 * @return true
+	 */
+	@Override
+	public boolean isVarArg() {
+		return true;
 
-    }
+	}
 }
