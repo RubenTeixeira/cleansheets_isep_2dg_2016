158513a0a8387ca674cfbd2be2fc09f0e9a8e3d1
1130383 <1130383@isep.ipp.pt>
2016-06-04 09:15:34 +0100

    LPFOURDG-120 #Comment new worklog for sprint2, started analysis and last update worklog sprint1 #time 30m

--- src/main/java/csheets/worklog/n1130383/sprint1/package-info.java
+++ src/main/java/csheets/worklog/n1130383/sprint1/package-info.java
@@ -3,10 +3,9 @@
  * Gomes during week1.
  *
  * <p>
- * <b>Scrum Master: no</b>
- *
+ * <b>Scrum Master:</b> no.
  * <p>
- * <b>Area Leader: no</b>
+ * <b>Area Leader:</b> no.
  *
  * <h2>1. Notes</h2>
  *
@@ -15,12 +14,17 @@
  * the mechanisms it uses to perform the already implemented functionality.
  * Besides the required intensive comprehesion and analysis, the team was
  * divided into groups to work on a Functional Increment. In ours team case, the
- * first requirement was Lang01.1 - Instructions Block. The first step was to
- * perform further analysis regarding all gramatical behaviour. Next the Design,
- * where possible Test solutions were designed and third the implementation. All
- * features were implemented as expected: Instructions Block where shown Value
- * should be the Value of the last operation in the block; Assign functionality
- * by assigning a Value to a specific Cell; For Function.</p>
+ * first requirement was Lang01.1 - Instructions Block.</p>
+ * <p>
+ * The first step was to perform further analysis regarding gramatical behaviour
+ * and how the application could deal with the input data. Next the Design,
+ * where possible Test solutions were designed and third the implementation.
+ * We've mostly coded using pair programming where two or more members were
+ * working on the same terminal exchange ideas on how to implement the
+ * functional requirement. All features were implemented as expected:
+ * Instructions Block where shown Value should be the Value of the last
+ * operation in the block; Assign functionality by assigning a Value to a
+ * specific Cell and the For Function.</p>
  *
  * <h2>2. Use Case/Feature: Lang01.1</h2>
  *
@@ -47,19 +51,26 @@
  * where the For Loop should end.</p>
  *
  * <p>
- * <b>Use Case "Instructions Block":</b>
- * Extend the formulas of Cleansheets.</p>
+ * <b>Use Case 1 - "Assign a refrenced cell with a value":</b> The user selects
+ * the cell where he/she wants to type an assignment operation, and then types
+ * it. The system updates the referenced cell with the resulting value.</p>
+ *
+ * <p>
+ * <b>Use Case 2 - "Instruction block":</b> The user writes down a set of
+ * instructions separated by a semi-colon and surrounded with bracket. The
+ * system all the instructions returning the resulting value from the last
+ * instruction.</p>
  *
  * <h2>4. Analysis</h2>
  * <p>
- * For the development of Use Case "Lang01.1 - Instructions Block", it's
- * necessary to analyze all classes of the project of all Formula Packages. We
- * conluded that is necessary to add new tokens "{", "}", ":=", "FOR" and define
- * new grammatical rules to recognize these tokens and their specific behaviour.
- * It is also necessary to create some extra classes such as class For
- * (implements Function), and Assign (implements Operator). In For Loop, the
- * first expression is the initialization, the second term is the boundary
- * condition, and the third is based on Instruction Blocks. The Class
+ * For the development of Use Case "Lang01.1 - Instructions Block" it's required
+ * to analyze all classes of the project of all Formula Packages. We conluded
+ * that is necessary to add new tokens "{", "}", ":=", "FOR" and define new
+ * grammatical rules to recognize these tokens and their specific behaviour. It
+ * is also necessary to create some extra classes such as class For (implements
+ * Function), and Assign (implements Operator). In For Loop, the first
+ * expression is the initialization, the second term is the boundary condition,
+ * and the third is based on Instruction Blocks. The Class
  * ExcelExpressionCompiler was fundamental to the understanding of the behaviour
  * on how to proceed. We will implement specific code to recognize the new
  * Tokens. This class implements methods that extract from the grammar a
@@ -67,10 +78,25 @@
  * tokens read from the input source (String). A Tree-shape formula (CommonTree)
  * is created containing the tokens which will be used for obtaining the correct
  * Cell final Value.</p>
+ *
+ * <h3>Assign Analysis:</h3>
  * <p>
- * Assign Analysis:
+ * Since an assignment differs from a BinaryOperation(Operand, Operator, Operand
+ * ) as well as from a UnaryOperation(Operator, operand), a new Operation must
+ * emerge: AssignmentOperation(CellReference, Operator, Operand). This will
+ * ensure the architecture will be ready for new assignment operations such as:
+ * '*= += /= ...' The following class diagram shows how we intend to implement
+ * this:</p>
+ * <img src="doc-files/class_analysis_lang01.1.png" alt="image">
  * <p>
- * <img src="doc-files/lang01.1_design_assign_operator.png" alt="Analysis">
+ * This approach was then abandoned as our team agreed with the Area Leader when
+ * he advised us not to extend the architecture yet, in favour of a more
+ * conservative approach. This would allow us to have a working solution in a
+ * more timely manner.</p>
+ *
+ * <h3>Instruction Block Analysis:</h3>
+ * <p>
+ * <img src="doc-files/lang01.1_design_assign_operator.png" alt="Analysis"></p>
  *
  * <h2>5.Design</h2>
  *
@@ -89,13 +115,16 @@
  * new grammar rules to accept new tokens, and a "for" loop with multiple
  * Expressions (itself an Instructions Block).</p>
  *
+ * <b>Assign Operation Sequence Diagram</b>:
+ * <img src="doc-files/lang01.1_design_assign_operator.png" alt="SD">
+ *
  * <h3>5.3. Classes</h3>
  * <p>
  * Class Diagram.</p>
  *
  * <h3>5.4. Design Patterns and Best Practices</h3>
  * <p>
- * Implemented Patterns promote Low Coupling - High Cohesion.</p>
+ * Implemented Patterns that promote Low Coupling - High Cohesion.</p>
  *
  *
  * <h2>6. Implementation</h2>
@@ -105,6 +134,22 @@
  * <p>
  * <b>Updated Classes/Files</b>: language.props, Formula.g,
  * ExcelExpressionCompiler.</p>
+ * <p>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/de7585f726fba1a7909a1c5db109f4880f12a6eb">Class
+ * Creation Commit</a></p>
+ * <p>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/e4e09359bc60855dac83cff747312d396003c3a0">Class
+ * Creation and IntructionBlock Implementation Commit as well as possible Test
+ * Solutions.</a></p>
+ * <p>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/7ecc655f858b81307defce06a350354542b73cb7">InstructionBlock
+ * Update Commit</a></p>
+ * <p>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/72d863fee9327bcc8c6f124a273fdbd30f2461bf">InstructionBlock
+ * Update as well as implementation ExcelExpressionCompiler</a></p>
+ * <p>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/44435553e11c99d77430d5fc02b9fb58035b3736">For
+ * Implementation Commit</a></p>
  *
  * <h2>7. Integration/Demonstration</h2>
  * <p>
@@ -113,60 +158,106 @@
  * <h2>8. Final Remarks</h2>
  *
  * <h2>9. Work Log</h2>
+ *
  * <p>
  * <b>Monday</b>
  * <p>
- * Yesterday I worked on:
- * <p>
- * 1. nothing
- * <p>
  * Today:
  * <p>
- * 1. Cloning of the project, its compilation, first analysis and reading
+ * - Cloning of the project, its compilation, first analysis and reading
  * Javadoc.
  * <p>
  * Blocking:
  * <p>
- * 1. nothing
+ * - nothing
+ *
  * <p>
  * <b>Tuesday</b>
  * <p>
  * Yesterday I worked on:
  * <p>
- * 1. Cloning of the project, its compilation and first analysis and reading
+ * - Cloning of the project, its compilation and first analysis and reading
  * Javadoc.
  * <p>
  * Today:
  * <p>
- * 1. Started my own worklog. Started analysis of Lang Functional Area and
- * Design on possible Test solutions.
+ * - Started my own worklog. Started analysis of Lang Functional Area and Design
+ * on possible Test solutions.
  * <p>
  * Blocking:
  * <p>
- * 1. nothing
+ * - nothing
+ *
+ *
  * <p>
  * <b>Wednesday</b>
  * <p>
+ * Yesterday I worked on:
+ * <p>
+ * - Started my own worklog. Started analysis of Lang Functional Area and Design
+ * on possible Test solutions.</p>
+ * <p>
  * Today:</p>
  * <p>
- * Implementation and Testing.</p>
+ * - Implementation of InstructionBlock Class and For Class as well as increased
+ * functionalitys on ExcelExpressionCompiler Class and Testing.
+ * </p>
+ * <p>
+ * Blocking:
+ * <p>
+ * - nothing
+ *
+ * <p>
  * <b>Thursday</b>
  * <p>
+ * Yesterday I worked on:
+ * <p>
+ * - Implementation of InstructionBlock Class and For Class as well as increased
+ * functionalitys on ExcelExpressionCompiler Class and Testing.</p>
+ * <p>
  * Today:</p>
  * <p>
- * Sprint Demonstration.</p>
+ * - Sprint Demonstration; Reunion with the Team to define (for the sprint nº2):
+ * further requirements that should and will be implemented, Scrum Master and
+ * Area Leader.</p>
+ * <p>
+ * Blocking:
+ * <p>
+ * - nothing
  *
+ *
+ *
+ * <p>
+ * <b>Friday</b>
+ * <p>
+ * Yesterday I worked on:</p>
+ * <p>
+ * - Sprint Demonstration; Reunion with the Team to define (for the sprint nº2):
+ * further requirements that should and will be implemented, Scrum Master and
+ * Area Leader.</p>
+ * <p>
+ * Today:</p>
+ * <p>
+ * - Reload Worklog; Prepare and Start the new Sprint, by creating it Tasks and
+ * Sub-Tasks.</p>
+ * <p>
+ * Blocking:
+ * <p>
+ * - nothing
  *
  * <h2>10. Self Assessment</h2>
  *
  * <h3>10.1. Design and Implementation:</h3>
  * <p>
- * 3- Good: The Tests cover a good portion of the Functional Requirements (over
- * 50%) and the code implemented follow the already existing patterns promoting
+ * 3- Good: The code implemented follow the already existing patterns promoting
  * Low Couple High Cohesion.</p>
  * <b>Evidences:</b>
+ * <p>
+ * Links provided above.</p>
  *
  * <h3>10.2. Teamwork</h3>
+ * <p>
+ * 3- Good.</p>
  *
  * <h3>10.3. Technical Documentation</h3>
  *
@@ -178,7 +269,7 @@ package csheets.worklog.n1130383.sprint1;
  * This class is only here so that javadoc includes the documentation about this
  * EMPTY package! Do not remove this class!
  *
- * @author alexandrebraganca
+ * @author Pedro Gomes 1130383@isep.ipp.pt
  */
 class _Dummy_ {
 }
---
+++ src/main/java/csheets/worklog/n1130383/sprint2/package-info.java
@@ -0,0 +1,251 @@
+/**
+ * Technical documentation regarding the work of the team member (1130383) Pedro
+ * Gomes during week2.
+ *
+ * <p>
+ * <b>Scrum Master:</b> no
+ *
+ * <p>
+ * <b>Area Leader:</b> yes
+ *
+ * <h2>1. Notes</h2>
+ *
+ * <p>
+ * -In this section you should register important notes regarding your work
+ * during the week. For instance, if you spend significant time helping a
+ * colleague or if you work in more than a feature-
+ *
+ * <h2>2. Use Case/Feature: Core02.1</h2>
+ *
+ * <b>Issue in Jira:</b>
+ * <p>
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-6">LPFOURDG-6</a></p>
+ * <b>Sub-tasks:</b>
+ * <p>
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-120">LPFOURDG-120</a></p>
+ * <p>
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-121">LPFOURDG-121</a></p>
+ * <p>
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-122">LPFOURDG-122</a></p>
+ * <p>
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-123">LPFOURDG-123</a></p>
+ *
+ *
+ * <h2>3. Requirement</h2>
+ * <p>
+ * A new extension to add sort functionalities to Cleansheets - Sort the
+ * contents of a column of cells. It should be possible to select the order:
+ * ascending or descending. The interaction with the user can be based solely on
+ * menu options. It should be possible to sort columns with numeric and/or text
+ * contents.</p>
+ *
+ * <p>
+ * <b>Use Case 1 - "Sort Column Ascending/Descending":</b> The user selects the
+ * column and what type of sort he wants (ascending/descending). The Extension
+ * proceeds to sort the column as required.</p>
+ *
+ *
+ * <h2>4. Analysis</h2>
+ * <p>
+ * The Analysis of the previous sprint relative to this area - Core01.1 -
+ * Enable/Disable Extensions - facilitates the understanding on how to implement
+ * this new Extension. As seen, the ExtensionManager.Class should load this
+ * Extension and provide it to the UiController. The ExtensionManager.Class is
+ * in charge of managing the extensions and it's the link between them and the
+ * Cleansheets application.</p>
+ * <b>ExtensionManager.Class</b>
+ * <p>
+ * Implements the Singleton pattern to guarantee that there is only one instance
+ * running. The Class links itself to a File - extension.props - that contains
+ * the name reference for all available extensions. All loaded Extensions are
+ * saved into a TreeMap with the Extensions Itself associated with a name
+ * reference (String).</p>
+ * <b>SortExtension.Class</b>
+ * <p>
+ * Therefore, an extension class should be implemented to support cell sorting.
+ * The class will extend, as all already implemented extensions,
+ * <b>Extension.class</b></p>
+ * <b>SortExtensionController.Class</b>
+ * <p>
+ * This Class will be implemented to handle sorting.</p>
+ *
+ *
+ * <h3>First Analysis sequence diagram</h3>
+ *
+ * <h3>Analysis of Core Technical Problem</h3>
+ *
+ * <h2>5. Design</h2>
+ *
+ * <h3>5.1. Functional Tests</h3>
+ *
+ * <h3>5.2. UC Realization</h3>
+ *
+ * <h3>Extension Setup</h3>
+ *
+ * <h3>5.3. Classes</h3>
+ *
+ * -Document the implementation with class diagrams illustrating the new and the
+ * modified classes-
+ *
+ * <h3>5.4. Design Patterns and Best Practices</h3>
+ *
+ * -Describe new or existing design patterns used in the issue-
+ * <p>
+ * -You can also add other artifacts to document the design, for instance,
+ * database models or updates to the domain model-
+ *
+ * <h2>6. Implementation</h2>
+ *
+ * -Reference the code elements that where updated or added-
+ * <p>
+ * -Also refer all other artifacts that are related to the implementation and
+ * where used in this issue. As far as possible you should use links to the
+ * commits of your work-</p>
+ *
+ * <h2>7. Integration/Demonstration</h2>
+ *
+ * -In this section document your contribution and efforts to the integration of
+ * your work with the work of the other elements of the team and also your work
+ * regarding the demonstration (i.e., tests, updating of scripts, etc.)-
+ *
+ * <h2>8. Final Remarks</h2>
+ *
+ * -In this section present your views regarding alternatives, extra work and
+ * future work on the issue.-
+ *
+ * <h2>9. Work Log</h2>
+ *
+ * <p>
+ * <b>Saturday</b>
+ * <p>
+ * Today:
+ * <p>
+ * 1.
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ *
+ * <p>
+ * <b>Sunday</b>
+ * <p>
+ * Yesterday I worked on:
+ * <p>
+ * 1.
+ * <p>
+ * Today:
+ * <p>
+ * 1.
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ *
+ * <p>
+ * <b>Monday</b>
+ * <p>
+ * Yesterday I worked on:
+ * <p>
+ * 1.
+ * <p>
+ * Today:
+ * <p>
+ * 1.
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ * <p>
+ * <b>Tuesday</b>
+ * <p>
+ * Yesterday I worked on:
+ * <p>
+ * 1.
+ * <p>
+ * Today:
+ * <p>
+ * 1.
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ *
+ * <p>
+ * <b>Wednesday</b>
+ * <p>
+ * Yesterday I worked on:
+ * <p>
+ * 1.</p>
+ * <p>
+ * Today:</p>
+ * <p>
+ * 1.</p>
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ * <p>
+ * <b>Thursday</b>
+ * <p>
+ * Yesterday I worked on:
+ * <p>
+ * 1.</p>
+ * <p>
+ * Today:</p>
+ * <p>
+ * 1.</p>
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ *
+ *
+ * <p>
+ * <b>Friday</b>
+ * <p>
+ * Yesterday I worked on:</p>
+ * <p>
+ * 1.</p>
+ * <p>
+ * Today:</p>
+ * <p>
+ * 1.</p>
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ * <h2>10. Self Assessment</h2>
+ *
+ * -Insert here your self-assessment of the work during this sprint.-
+ *
+ * <h3>10.1. Design and Implementation</h3>
+ *
+ * <p>
+ * <b>Evidences:</b>
+ * <p>
+ * - url of commit: - description: this commit is related to the implementation
+ * of the design pattern-</p>
+ *
+ * <h3>10.2. Teamwork:</h3>
+ *
+ * <h3>10.3. Technical Documentation:</h3>
+ *
+ * @author Pedro Gomes 1130383@isep.ipp.pt
+ */
+package csheets.worklog.n1130383.sprint2;
+
+/**
+ * This class is only here so that javadoc includes the documentation about this
+ * EMPTY package! Do not remove this class!
+ *
+ * @author Pedro Gomes 1130383@isep.ipp.pt
+ */
+class _Dummy_ {
+}
