ce704aaaf5363ed66ce92252fb65fa22ffc93a43
Renato Machado <1140423@isep.ipp.pt>
2016-06-08 23:57:31 +0100

    #LPFOURDG-66 - Implemented Channel Wildcards. Improved feature design. Updated worklog.

--- src/main/java/csheets/worklog/n1140423/sprint2/package-info.java
+++ src/main/java/csheets/worklog/n1140423/sprint2/package-info.java
@@ -9,6 +9,7 @@
  * 
  * <h2>1. Notes</h2>
  * 
+ * Helped my colleagues throughout the sprint in regards to Volt and network/thread related questions.
  * 
  * <h2>2. Use Case/Feature: IPC06.1</h2>
  * 
@@ -76,39 +77,60 @@
  * This means that any extension that currently uses the IPC Protocol Services, should be able to secure their messages, thus allow me to get information on what it's sent.
  * </p>
  * 
- * <h3>Tests:</h3>
- * <p>
- * Since we're encrypting a message, that should be tested to check proper encryption and decryption.
- * As for network related operations such as transmitting a message through UDP and TCP are hard to test since we'd need to have another host on our network to test (since we block ourselves on a broadcast signal),
- * which is a flaw since the build of the project would fail most of the time due to failed connections or not having other hosts. Therefore, all tests based on network related operations will only be available through
- * screenshots to prove what the correct I/O should be (unless the feature requirements change).
- * </p>
- * 
  * <h3>Sequence Diagram:</h3>
- * 
+ *
  * <p>
- * Since any feature can use this feature, we can generalize the User Interface class (UI) and the Controller class (Controller). The reason for this is because
- * any other feature can implement their own UI and controller while using the same process to implement secure message transmission.
+ * Since any feature can use this feature, we can generalize the User Interface
+ * class (UI) and the Controller class (Controller). The reason for this is
+ * because any other feature can implement their own UI and controller while
+ * using the same process to implement secure message transmission.
  * </p>
- * 
+ *
  * <p>
- * <strong>Important:</strong> Since all network based features must implement the protocols services (such as UdpService or TcpService), this diagram will focus
- * solely on showing on how to trigger secure messaging, instead of showing the whole setting up the service process all over.
+ * <strong>Important:</strong> Since all network based features must implement
+ * the protocols services (such as UdpService or TcpService), this diagram will
+ * focus solely on showing on how to trigger secure messaging, instead of
+ * showing the whole setting up the service process all over.
  * </p>
  * <p>
- * <strong>Note:</strong> It is also important to note that the Service is abstracted to work with the UDP or TCP protocol, which means that the class represented as the "Service" in the
- * sequence diagram can be either UdpService or TcpService.
+ * <strong>Note:</strong> It is also important to note that the Service is
+ * abstracted to work with the UDP or TCP protocol, which means that the class
+ * represented as the "Service" in the sequence diagram can be either UdpService
+ * or TcpService.
  * </p>
- * 
+ *
  * <h4>Server</h4>
  * <img src="doc-files/general_design_server.png" alt="Server generalization">
- * 
+ *
  * <h4>Client</h4>
  * <img src="doc-files/general_design_client.png" alt="Client generalization">
  * 
+ * <b>Update</b>: Since the last few days I've written a unified service to serve our extension needs, I've decided to update the design diagram.
+ * It is a bit more complete, but essentially it's nearly identical as we just have to declare the channel Volt is filtrating the message and expect for a route.
+ * 
+ * <img src="doc-files/final_design.png" alt="Final Design">
+ * 
+ * <h3>Tests:</h3>
+ * <p>
+ * Since we're encrypting a message, that should be tested to check proper encryption and decryption.
+ * We are going to manually test the network to see expected results.
+ * </p>
+ * 
+ * 
  * <h2>6. Implementation</h2>
  * 
+ * <p>
+ * In regards to implementation, most of the feature implementation is done through Volt. Since Volt already supports Channels for Encryption and Decryption and as well as 
+ * watch who receives and sends messages, the feature is easily implemented.
+ * </p>
+ * <p>
+ * Channels are a way to filter the message input & output in certain points of Volt's execution. The Channels have 2 hook methods which are before() and after(), which allows us to execute
+ * actions before and after the Volt execution. This allows for some very powerful data manipulation, yet incredibly simple.
+ * </p>
  * 
+ * <p>
+ * As for Encryption, Volt uses AES-128 bits encryption which requires a key to both encrypt and decrypt messages. This class is available in Volt's package.
+ * </p>
  * <h2>7. Integration/Demonstration</h2>
  * 
  * <h2>8. Final Remarks</h2>
@@ -121,6 +143,7 @@
  * <p>Started to plan out Volt new features. Started and completed the first analysis on this feature.</p>
  * <p>Blocking</p>
  * <p>Nothing.</p>
+ * 
  * <p>06/06/2016</p>
  * <p>Monday</p>
  * <p>Today</p>
@@ -128,12 +151,27 @@
  * <p>Blocking</p>
  * <p>Nothing</p>
  * 
+ * <p>07/06/2016</p>
+ * <p>Tuesday</p>
+ * <p>Today</p>
+ * <p>Going to work a bit on unifying the multiple network services into one service. Going to make some improvements into Volt.</p>
+ * <p>Blocking</p>
+ * <p>Nothing</p>
+ * 
+ * <p>08/06/2016</p>
+ * <p>Wednesday</p>
+ * <p>Yesterday</p>
+ * <p>Implemented the Volt static class and made some small improvements to it.</p>
+ * <p>Today</p>
+ * <p>Will finish my use case by implementing the User Interface and the associated controller following the design. Test my use case and update the overall documentation.</p>
+ * <p>Blocking</p>
+ * <p>Nothing</p>
  * 
  * <h2>10. Self Assessment</h2> 
  * 
- * Outcome 3 ("Design and Implementation") - ?
- * Outcome 5 ("Teamwork") - ?
- * Outcome 6 ("Technical Documentation") - ?
+ * <p>Outcome 3 ("Design and Implementation") -4</p>
+ * <p>Outcome 5 ("Teamwork") - 3</p>
+ * <p>Outcome 6 ("Technical Documentation") - 4</p>
  *
  * <h3>10.1. Design and Implementation:3</h3>
  * 
@@ -144,9 +182,12 @@
  * <b>Evidences:</b>
  * </p>
  * <p>
- * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/b8cf8e3950952c780501d4618af5b4626b086452">Analysis</a>
- * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/41c3c827ab8a71cbcf35f881f617e5534537eb84">Base design</a>
- * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/941200bbd1397d028114571cfaf36d3c0bf80e20">Improved Volt performance and documentation. Added new features to Volt: Channels and Dependencies. Added Encrypter.</a>
+ * <p><a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/b8cf8e3950952c780501d4618af5b4626b086452">Analysis</a></p>
+ * <p><a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/41c3c827ab8a71cbcf35f881f617e5534537eb84">Base design</a></p>
+ * <p><a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/941200bbd1397d028114571cfaf36d3c0bf80e20">Improved Volt performance and documentation. Added new features to Volt: Channels and Dependencies. Added Encrypter.</a></p>
+ * <p><a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/4f27d5ab684c3fbb892412d6ba8d8ef9887c2811">Added the ability for Volt to handle multiple concurrent services.</a></p>
+ * <p><a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/f1f002968d1da85a6d73db628982edaf9777a861">Updated feature UI and Controller. Updated Volt.</a></p>
+ * <p><a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/3c6e1ab1a01b3d9e20f8864ed7096c855eabee7e">Added the NetworkManager</a></p>
  * </p>
  * 
  * <h3>10.2. Teamwork: ...</h3>
