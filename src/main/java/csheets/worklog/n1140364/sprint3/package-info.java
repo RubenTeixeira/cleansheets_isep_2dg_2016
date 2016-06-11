284d04a680e36a7292275b07a4e93fc6647acac7
Jose_Barros <1140364@isep.ipp.pt>
2016-06-11 16:15:10 +0100

    worklog updated and small change in javadoc

---
+++ src/main/java/csheets/worklog/n1140364/sprint3/package-info.java
@@ -0,0 +1,233 @@
+/**
+ * Technical documentation regarding the work of the team member (1140364) José
+ * Barros during week3.
+ *
+ * <p>
+ * <b>Scrum Master:</b> no
+ *
+ * <p>
+ * <b>Area Leader:</b> yes
+ *
+ * <h2>1. Notes</h2>
+ *
+ * <p>
+ * -In this section you should register important notes regarding your work
+ * during the week. For instance, if you spend significant time helping a
+ * colleague or if you work in more than a feature-
+ *
+ * <h2>2. Use Case/Feature: Lang06.2</h2>
+ *
+ * <b>Issue in Jira:</b>
+ * <p>
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-43">LPFOURDG-43</a></p>
+ * <b>Sub-tasks:</b>
+ * <p>
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-">LPFOURDG-</a></p>
+ * <p>
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-">LPFOURDG-</a></p>
+ * <p>
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-">LPFOURDG-</a></p>
+ * <p>
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-">LPFOURDG-</a></p>
+ *
+ *
+ * <h2>3. Requirement</h2>
+ * <p>
+ * An extension that adds support for Forms. Forms are windows that are designed
+ * by the end user and can be integrated in languages. They are a simple way to
+ * leverage languages by providing a mechanism to integrate user input/output
+ * into languages (such as the Cleansheets formulas or macros).</p>
+ *
+ * <p>
+ * <b>Use Case 2 - "Forms and Variables":</b> In order for forms to become
+ * useful in formulas or macros it is necessary to associate data with the
+ * contents of the visual widgets. The mechanism used for that will be the
+ * binding of variables (macros or formulas variables) with the contents of the
+ * visual widgets. One simple way to achieve this is by using temporary
+ * variables (from macros and formulas). The matching between widgets and
+ * variables should be done by associating the ones with the same name. When
+ * displaying a form (in the context of a macro or a formula), if the temporary
+ * variables with the same name of widgets exist, them they are used to set the
+ * content of the widgets. For widgets for which no temporary variables with the
+ * same name are found then new temporary variables should be created. The user
+ * should be able to change the contents of edit boxes. When closing the form
+ * window the contents of the temporary variables should be updated from the
+ * contents of the corresponding visual widgets.</p>
+ *
+ *
+ * <h2>4. Analysis</h2>
+ * Analysis description...
+ *
+ * <h3>First Analysis sequence diagram</h3>
+ *
+ * <h3>#</h3>
+ * Step description...
+ *
+ * <p>
+ * <img src="#" alt="image">
+ *
+ *
+ * <h3>Analysis of Core Technical Problem</h3>
+ *
+ * <h2>5. Design</h2>
+ * ...
+ *
+ * <h3>5.1. Functional Tests</h3>
+ * Test description...
+ *
+ * <p>
+ * see: <code>#</code>
+ * </p>
+ *
+ * <h3>5.2. UC Realization</h3>
+ * UC Realization description...
+ *
+ * <h3>5.3. Classes</h3>
+ * ...
+ *
+ * <h3>5.4. Design Patterns and Best Practices</h3>
+ *
+ * -Describe new or existing design patterns used in the issue-
+ * <p>
+ * -You can also add other artifacts to document the design, for instance,
+ * database models or updates to the domain model-
+ *
+ * <h2>6. Implementation</h2>
+ *
+ * -Reference the code elements that where updated or added-
+ * <p>
+ * -Also refer all other artifacts that are related to the implementation and
+ * where used in this issue. As far as possible you should use links to the
+ * commits of your work-</p>
+ *
+ * <h2>7. Integration/Demonstration</h2>
+ *
+ * -In this section document your contribution and efforts to the integration of
+ * your work with the work of the other elements of the team and also your work
+ * regarding the demonstration (i.e., tests, updating of scripts, etc.)-
+ *
+ * <h2>8. Final Remarks</h2>
+ *
+ * -In this section present your views regarding alternatives, extra work and
+ * future work on the issue.-
+ *
+ * <h2>9. Work Log</h2>
+ *
+ * <p>
+ * <b>Saturday</b>
+ * <p>
+ * Today:
+ * <p>
+ * 1. Analysis use case and design.</p>
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ *
+ * <p>
+ * <b>Sunday</b>
+ * <p>
+ * Yesterday I worked on:
+ * <p>
+ * 1.</p>
+ * <p>
+ * Today:
+ * <p>
+ * 1.</p>
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ *
+ * <p>
+ * <b>Monday</b>
+ * <p>
+ * Yesterday I worked on:
+ * <p>
+ * 1.</p>
+ * <p>
+ * Today:
+ * <p>
+ * 1.</p>
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ * <p>
+ * <b>Tuesday</b>
+ * <p>
+ * Yesterday I worked on:
+ * <p>
+ * 1.</p>
+ * <p>
+ * Today:
+ * <p>
+ * 1.</p>
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ *
+ * <p>
+ * <b>Wednesday</b>
+ * <p>
+ * Yesterday I worked on:
+ * <p>
+ * 1.</p>
+ * <p>
+ * Today:</p>
+ * <p>
+ * 1.</p>
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ * <p>
+ * <b>Thursday</b>
+ * <p>
+ * Yesterday I worked on:
+ * <p>
+ * 1.</p>
+ * <p>
+ * Today:</p>
+ * <p>
+ * 1.</p>
+ * <p>
+ * Blocking:
+ * <p>
+ * 1.
+ *
+ *
+ * <h2>10. Self Assessment</h2>
+ *
+ * -Insert here your self-assessment of the work during this sprint.-
+ *
+ * <h3>10.1. Design and Implementation</h3>
+ *
+ * <p>
+ * <b>Evidences:</b>
+ * <p>
+ * - url of commit: - description: this commit is related to the implementation
+ * of the design pattern-</p>
+ *
+ * <h3>10.2. Teamwork:</h3>
+ *
+ * <h3>10.3. Technical Documentation:</h3>
+ *
+ * @author José Barros 1140364@isep.ipp.pt
+ */
+package csheets.worklog.n1140364.sprint3;
+
+/**
+ * This class is only here so that javadoc includes the documentation about this
+ * EMPTY package! Do not remove this class!
+ *
+ * @author José Barros 1140364@isep.ipp.pt
+ */
+class _Dummy_ {
+}
