ace3781d5c72562fc6537d8bf554d35da38b1de6
1141330 <1141330@isep.ipp.pt>
2016-06-13 17:11:44 +0100

    #LPFOURDG-34 Analysis of my UC, and some debug.

---
+++ src/main/java/csheets/worklog/n1141330/sprint3/package-info.java
@@ -0,0 +1,78 @@
+/**
+ * Technical documentation regarding the work of the team member (1141330) Eduardo Marques during week3.
+ *
+ *
+ * <b>Scrum Master: -(yes/no)- no</b>
+ *
+ *
+ * <b>Area Leader: -(yes/no)- no</b>
+ *
+ * <h2>1. Notes</h2>
+ *
+ * -Notes about the week's work.-
+ *
+ * First Day analyse the problem, SD Diagram. And debug the project for know
+ * what class could give a lista of selected cell's
+ * <h2>2. Use Case/Feature: CRM5.1</h2>
+ *
+ * Issue in Jira:
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-34">LPFOURDG-34</a>
+ *
+ *
+ * <h2>3. Requirement</h2>
+ *
+ * <b>Use Case "Conditional Formatting of Ranges":</b> Cleansheets should now
+ * can apply a StylableCell to a list of selected cells. If the selected cells
+ * are greater than a number it should apply a style to the cells, if the number
+ * is less apply a different style
+ *
+ *
+ * <h2>4. Analysis</h2>
+ * <img src="doc-files/Lang03.2-analyse.png" alt="image">
+ *
+ * <h2>5. Design</h2>
+ *
+ *
+ * Diagram of extention state change
+ *
+ * <h3>5.3. Classes</h3>
+ *
+ * <h2>9. Work Log</h2>
+ *
+ * -Insert here a log of you daily work. This is in essence the log of your
+ * daily standup meetings.-
+ *
+ * Example
+ *
+ * <b>Monday</b>
+ *
+ * Monday i worked on: Devoloped the analyse and know what class should we work
+ * and how i get a value of a selected cell.
+ * <b>Tuesday</b>
+ *
+ * Yesterday I worked on:
+ * <h2>10. Self Assessment</h2>
+ *
+ *
+ * <h3>10.1. Design and Implementation:3</h3>
+ *
+ *
+ * <b>Evidences:</b>
+ *
+ *
+ * <h3>10.2. Teamwork: ...</h3>
+ *
+ * <h3>10.3. Technical Documentation: ...</h3>
+ *
+ * @author Eduardo Marques
+ */
+package csheets.worklog.n1141330.sprint3;
+
+/**
+ * This class is only here so that javadoc includes the documentation about this
+ * EMPTY package! Do not remove this class!
+ *
+ * @author Eduardo Marques
+ */
+class _Dummy_ {
+}
