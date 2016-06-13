5bb8f6cf3b6c0019bfebd5c54967597a8c56e0e2
AB-1140280 <1140280@isep.ipp.pt>
2016-06-13 14:27:26 +0100

    Uncommenting tests

--- src/test/java/csheets/ext/agenda/ui/AgendaControllerTest.java
+++ src/test/java/csheets/ext/agenda/ui/AgendaControllerTest.java
@@ -5,62 +5,73 @@
  */
 package csheets.ext.agenda.ui;
 
+import csheets.domain.Contact;
+import csheets.persistence.PersistenceContext;
+import csheets.ui.ctrl.UIController;
+import java.util.ArrayList;
+import java.util.Calendar;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
 /**
  *
  * @author ab
  */
 public class AgendaControllerTest {
 
-//    AgendaController controller;
-//
-//    public AgendaControllerTest() {
-//        UIController uiController = UIController.getUIController();
-//        controller = new AgendaController(uiController);
-//
-//    }
-//
-//    /**
-//     * Test of getContacts method, of class AgendaController.
-//     */
-//    @Test
-//    public void testGetContacts() {
-//        ArrayList<Contact> list = new ArrayList();
-//        for (Contact contact : PersistenceContext.repositories().contacts().
-//                all()) {
-//            list.add(contact);
-//        }
-//        ContactListModel expectedResult = new ContactListModel(list);
-//        ContactListModel result = controller.getContacts();
-//        Assert.assertEquals(expectedResult, result);
-//    }
-//
-//    @Test
-//    public void testFailGetContacts() {
-//        ArrayList<Contact> list = new ArrayList();
-//        ContactListModel expectedResult = new ContactListModel(list);
-//        ContactListModel result = controller.getContacts();
-//        assertNotEquals(expectedResult, result);
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void testNullGetContacts() {
-//        ContactListModel expectedResult = new ContactListModel(null);
-//        ContactListModel result = controller.getContacts();
-//        assertTrue(false);
-//    }
-//
-//    /**
-//     * Test of updateEvents method, of class AgendaController.
-//     */
-//    @Test
-//    public void testUpdateEvents() {
-//        ContactListModel list = controller.getContacts();
-//        if (list.isEmpty()) {
-//            assertTrue(true);
-//        }
-//        Contact cont = list.getContactAt(0);
-//        Iterable result = controller.updateEvents(Calendar.getInstance(), cont);
-//        Iterable expected = PersistenceContext.repositories().events().eventsContactPerDay(cont, Calendar.getInstance());
-//        assertEquals(expected, result);
-//    }
+    AgendaController controller;
+
+    public AgendaControllerTest() {
+        UIController uiController = UIController.getUIController();
+        controller = new AgendaController(uiController);
+
+    }
+
+    /**
+     * Test of getContacts method, of class AgendaController.
+     */
+    @Test
+    public void testGetContacts() {
+        ArrayList<Contact> list = new ArrayList();
+        for (Contact contact : PersistenceContext.repositories().contacts().
+                all()) {
+            list.add(contact);
+        }
+        ContactListModel expectedResult = new ContactListModel(list);
+        ContactListModel result = controller.getContacts();
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testFailGetContacts() {
+        ArrayList<Contact> list = new ArrayList();
+        ContactListModel expectedResult = new ContactListModel(list);
+        ContactListModel result = controller.getContacts();
+        assertNotEquals(expectedResult, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNullGetContacts() {
+        ContactListModel expectedResult = new ContactListModel(null);
+        ContactListModel result = controller.getContacts();
+        assertTrue(false);
+    }
+
+    /**
+     * Test of updateEvents method, of class AgendaController.
+     */
+    @Test
+    public void testUpdateEvents() {
+        ContactListModel list = controller.getContacts();
+        if (list.isEmpty()) {
+            assertTrue(true);
+        }
+        Contact cont = list.getContactAt(0);
+        Iterable result = controller.updateEvents(Calendar.getInstance(), cont);
+        Iterable expected = PersistenceContext.repositories().events().eventsContactPerDay(cont, Calendar.getInstance());
+        assertEquals(expected, result);
+    }
 }
