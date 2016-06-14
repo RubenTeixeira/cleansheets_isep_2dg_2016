1e383eaa5f0fc31ceeb8130a6c12ede6402252ee
João Miguel Faria Martins <1140263@isep.ipp.pt>
2016-06-14 02:43:17 +0100

    LPFOURDG-236 #time 30m Work log First considerations - requirements and analysis.

--- src/main/java/csheets/worklog/n1140263/sprint3/package-info.java
+++ src/main/java/csheets/worklog/n1140263/sprint3/package-info.java
@@ -2,133 +2,66 @@
  * Technical documentation regarding the work of the team member (1140263) Joao Martins during week3
  *
  * <p>
- * <b>-Note: this is a template/example of the individual documentation that
- * each team member must produce each week/sprint. Suggestions on how to build
- * this documentation will appear between '-' like this one. You should remove
- * these suggestions in your own technical documentation-</b>
- * <p>
- * <b>Scrum Master: -(yes/no)- yes</b>
+ * <b>Scrum Master: -(yes/no)- no</b>
  *
  * <p>
- * <b>Area Leader: -(yes/no)- yes</b>
+ * <b>Area Leader: -(yes/no)- no</b>
  *
  * <h2>1. Notes</h2>
  *
  * -Notes about the week's work.-
  * <p>
- * -In this section you should register important notes regarding your work
- * during the week. For instance, if you spend significant time helping a
- * colleague or if you work in more than a feature.-
+ * - notas se existiu ajuda aos colegas -
  *
- * <h2>2. Use Case/Feature: Core02.1</h2>
+ * <h2>2. Use Case/Feature: Core06.2Forms and variables</h2>
  *
- * Issue in Jira: -place here a link to the jira issue-
+ * Issue in Jira:
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-43">LPFOURDG-43</a>
  * <p>
- * -Include the identification and description of the feature-
+ * Sub-Task in Jira:
+ * <p>
+ * Analysis:
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-236">LPFOURDG-236</a>
+ * <p>
+ * Design:
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-237">LPFOURDG-237</a>
+ * <p>
+ * Implementation:
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-238">LPFOURDG-238</a>
+ * <p>
+ * Tests:
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-239">LPFOURDG-239</a>
+ *
  *
  * <h2>3. Requirement</h2>
- * Setup extension for comments on cells. The user should be able to activate
- * and deactivate comments on cells. When activated, a sidebar for the comments
- * should appear. The sidebar should be composed of a simple textbox to display
- * and edit a comment. At the moment it is not required to save comments to
- * disk.
- *
- * <p>
- * <b>Use Case "Enter Comment on Cell":</b> The user selects the cell where
- * he/she wants to enter a comment. The system displays the current comment of
- * that cell. The user enter the text of the comment (or alters the existing
- * one). The system saves the comment of the cell.
+ * In order for forms to become useful in formulas or macros it is necessary to
+ * associate data with the contents of the visual widgets. The mechanism used
+ * for that will be the binding of variables (macros or formulas variables) with
+ * the contents of the visual widgets. One simple way to achieve this is by
+ * using temporary variables (from macros and formulas). The matching between
+ * widgets and variables should be done by associating the ones with the same
+ * name. When displaying a form (in the context of a macro or a formula), if the
+ * temporary variables with the same name of widgets exist, them they are used
+ * to set the content of the widgets. For widgets for which no temporary
+ * variables with the same name are found then new temporary variables should be
+ * created. The user should be able to change the contents of edit boxes. When
+ * closing the form window the contents of the temporary variables should be
+ * updated from the contents of the corresponding visual widgets.
  *
  *
  * <h2>4. Analysis</h2>
- * Since comments on cells will be supported in a new extension to cleansheets
- * we need to study how extensions are loaded by cleansheets and how they work.
- * The first sequence diagram in the section
- * <a href="../../../../overview-summary.html#arranque_da_aplicacao">Application
- * Startup</a> tells us that extensions must be subclasses of the Extension
- * abstract class and need to be registered in special files. The Extension
- * class has a method called getUIExtension that should be implemented and
- * return an instance of a class that is a subclass of UIExtension. In this
- * subclass of UIExtension there is a method (getSideBar) that returns the
- * sidebar for the extension. A sidebar is a JPanel.
+ * Allow the user to create a form dynamically in it can insert or remove a line
+ * with buttons or labels or textfields, which is composed of a name and
+ * content. Use temporary variables. It is necessary to associate data with the
+ * contents of the visual widgets. The user should be able to change the
+ * contents of edit boxes. When closing the form window the contents of the
+ * temporary variables should be updated from the contents of the corresponding
+ * visual widgets.
  *
  *
  * <h3>First "analysis" sequence diagram</h3>
- * The following diagram depicts a proposal for the realization of the
- * previously described use case. We call this diagram an "analysis" use case
- * realization because it functions like a draft that we can do during analysis
- * or early design in order to get a previous approach to the design. For that
- * reason we mark the elements of the diagram with the stereotype "analysis"
- * that states that the element is not a design element and, therefore, does not
- * exists as such in the code of the application (at least at the moment that
- * this diagram was created).
- * <p>
- * <img src="doc-files/comments_extension_uc_realization1.png" alt="image">
- * <p>
+ * imagem - ssd
  *
- * From the previous diagram we see that we need to add a new "attribute" to a
- * cell: "comment". Therefore, at this point, we need to study how to add this
- * new attribute to the class/interface "cell". This is the core technical
- * problem regarding this issue.
- * <h3>Analysis of Core Technical Problem</h3>
- * We can see a class diagram of the domain model of the application
- * <a href="../../../../overview-summary.html#modelo_de_dominio">here</a>
- * From the domain model we see that there is a Cell interface. This defines the
- * interface of the cells. We also see that there is a class CellImpl that must
- * implement the Cell interface. If we open the {@link csheets.core.Cell} code
- * we see that the interface is defined as:
- * <code>public interface Cell extends Comparable &lt;Cell&gt;, Extensible&lt;Cell&gt;, Serializable</code>.
- * Because of the <code>Extensible</code> it seams that a cell can be extended.
- * If we further investigate the hierarchy of {@link csheets.core.Cell} we see
- * that it has a subclass {@link csheets.ext.CellExtension} which has a subclass
- * {@link csheets.ext.style.StylableCell}.
- * {@link csheets.ext.style.StylableCell} seems to be an example of how to
- * extend cells. Therefore, we will assume that it is possible to extend cells
- * and start to implement tests for this use case.
- * <p>
- * The <a href="http://en.wikipedia.org/wiki/Delegation_pattern">delegation
- * design pattern</a> is used in the cell extension mechanism of cleansheets.
- * The following class diagram depicts the relations between classes in the
- * "Cell" hierarchy.
- * <p>
- * <img src="doc-files/core02_01_analysis_cell_delegate.png" alt="image">
- *
- * <p>
- * One important aspect is how extensions are dynamically created and returned.
- * The <code>Extensible</code> interface has only one method,
- * <code>getExtension</code>. Any class, to be extensible, must return a
- * specific extension by its name. The default (and base) implementation for the
- * <code>Cell</code> interface, the class <code>CellImpl</code>, implements the
- * method in the following manner:
- * <pre>
- * {@code
- * 	public Cell getExtension(String name) {
- *		// Looks for an existing cell extension
- *		CellExtension extension = extensions.get(name);
- *		if (extension == null) {
- *			// Creates a new cell extension
- *			Extension x = ExtensionManager.getInstance().getExtension(name);
- *			if (x != null) {
- *				extension = x.extend(this);
- *				if (extension != null)
- *					extensions.put(name, extension);
- *			}
- *		}
- *		return extension;
- *	}
- * }
- * </pre> As we can see from the code, if we are requesting a extension that is
- * not already present in the cell, it is applied at the moment and then
- * returned. The extension class (that implements the <code>Extension</code>
- * interface) what will do is to create a new instance of its cell extension
- * class (this will be the <b>delegator</b> in the pattern). The constructor
- * receives the instance of the cell to extend (the <b>delegate</b> in the
- * pattern). For instance, <code>StylableCell</code> (the delegator) will
- * delegate to <code>CellImpl</code> all the method invocations regarding
- * methods of the <code>Cell</code> interface. Obviously, methods specific to
- * <code>StylableCell</code> must be implemented by it. Therefore, to implement
- * a cell that can have a associated comment we need to implement a class
- * similar to <code>StylableCell</code>.
  *
  * <h2>5. Design</h2>
  *
@@ -231,11 +164,11 @@
  * <p>
  * Yesterday I worked on:
  * <p>
- * 1. -nothing-
+ * 1. Learn 6.1 Lang implementation.
  * <p>
  * Today
  * <p>
- * 1. Analysis of the...
+ * 1. Analysis. 2. Think about design.
  * <p>
  * Blocking:
  * <p>
@@ -261,10 +194,6 @@
  *
  * <h3>10.1. Design and Implementation:3</h3>
  *
- * 3- bom: os testes cobrem uma parte significativa das funcionalidades (ex:
- * mais de 50%) e apresentam código que para além de não ir contra a arquitetura
- * do cleansheets segue ainda as boas práticas da área técnica (ex:
- * sincronização, padrões de eapli, etc.)
  * <p>
  * <b>Evidences:</b>
  * <p>
@@ -275,7 +204,7 @@
  *
  * <h3>10.3. Technical Documentation: ...</h3>
  *
- * @author alexandrebraganca
+ * @author joao martins
  */
 package csheets.worklog.n1140263.sprint3;
 
@@ -283,7 +212,7 @@ package csheets.worklog.n1140263.sprint3;
  * This class is only here so that javadoc includes the documentation about this
  * EMPTY package! Do not remove this class!
  *
- * @author alexandrebraganca
+ * @author joao martins
  */
 class _Dummy_ {
 }
