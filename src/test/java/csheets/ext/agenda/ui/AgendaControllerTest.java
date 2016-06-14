71930d508ea6823e0547575ba878246a936eb128
AB-1140280 <1140280@isep.ipp.pt>
2016-06-14 20:13:35 +0100

    LPFOURDG-242	#resolve #time 30m #comment Finished testing, 100% coverage on core controller functionalities

--- src/test/java/csheets/ext/agenda/ui/AgendaControllerTest.java
+++ src/test/java/csheets/ext/agenda/ui/AgendaControllerTest.java
@@ -7,13 +7,20 @@ package csheets.ext.agenda.ui;
 
 import csheets.domain.CompanyContact;
 import csheets.domain.Contact;
+import csheets.domain.ContactCalendar;
+import csheets.domain.Event;
 import csheets.domain.PersonContact;
 import csheets.framework.persistence.repositories.DataIntegrityViolationException;
 import csheets.persistence.PersistenceContext;
 import csheets.ui.ctrl.UIController;
+import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Calendar;
 import org.junit.After;
 import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -26,9 +33,20 @@ public class AgendaControllerTest {
 	AgendaController controller;
 	PersonContact contact1;
 	PersonContact contact2;
+	ContactCalendar cal;
+	Event event1;
+	Event event2;
+	Event event3;
 	CompanyContact c;
+	Calendar startDate;
+	Calendar endDate;
 
 	public AgendaControllerTest() throws DataIntegrityViolationException {
+		startDate = Calendar.getInstance();
+		startDate.add(Calendar.YEAR, 5);
+		endDate = Calendar.getInstance();
+		endDate.add(Calendar.YEAR, 5);
+		endDate.add(Calendar.DAY_OF_MONTH, 5);
 		UIController uiController = UIController.getUIController();
 		controller = new AgendaController(uiController);
 		byte[] photo = {};
@@ -38,6 +56,19 @@ public class AgendaControllerTest {
 		PersistenceContext.repositories().contacts().add(contact1);
 		contact2 = new PersonContact("testtesttest2", "testtesttest2", "testtesttest2", c, photo);
 		PersistenceContext.repositories().contacts().add(contact2);
+		cal = new ContactCalendar("testeste", "testeste", Color.yellow, contact2);
+		PersistenceContext.repositories().calendars().add(cal);
+		event1 = new Event(cal, "testeste", startDate, endDate);
+		PersistenceContext.repositories().events().add(event1);
+		event2 = new Event(cal, "teffsteste", Calendar.getInstance(), Calendar.
+						   getInstance());
+		PersistenceContext.repositories().events().add(event2);
+		Calendar date1 = Calendar.getInstance();
+		Calendar endDate2 = Calendar.getInstance();
+		endDate2.add(Calendar.YEAR, 5);
+		endDate2.add(Calendar.DAY_OF_MONTH, 10);
+		event3 = new Event(cal, "teffstesweete", Calendar.getInstance(), endDate2);
+		PersistenceContext.repositories().events().add(event3);
 
 	}
 
@@ -47,6 +78,9 @@ public class AgendaControllerTest {
 
 	@After
 	public void tearDown() {
+		PersistenceContext.repositories().events().delete(event1);
+		PersistenceContext.repositories().events().delete(event2);
+		PersistenceContext.repositories().calendars().delete(cal);
 		PersistenceContext.repositories().contacts().delete(contact1);
 		PersistenceContext.repositories().contacts().delete(contact2);
 		PersistenceContext.repositories().contacts().delete(c);
@@ -67,34 +101,85 @@ public class AgendaControllerTest {
 		ContactListModel result = controller.getContacts();
 		Assert.assertEquals(expectedResult, result);
 	}
-//
-//    @Test
-//    public void testFailGetContacts() {
-//        ArrayList<Contact> list = new ArrayList();
-//        ContactListModel expectedResult = new ContactListModel(list);
-//        ContactListModel result = controller.getContacts();
-//        assertNotEquals(expectedResult, result);
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void testNullGetContacts() {
-//        ContactListModel expectedResult = new ContactListModel(null);
-//        ContactListModel result = controller.getContacts();
-//        assertTrue(false);
-//    }
-//
-//    /**
-//     * Test of updateEvents method, of class AgendaController.
-//     */
-//    @Test
-//    public void testUpdateEvents() {
-//        ContactListModel list = controller.getContacts();
-//        if (list.isEmpty()) {
-//            assertTrue(true);
-//        }
-//        Contact cont = list.getContactAt(0);
-//        Iterable result = controller.updateEvents(Calendar.getInstance(), cont);
-//        Iterable expected = PersistenceContext.repositories().events().eventsContactPerDay(cont, Calendar.getInstance());
-//        assertEquals(expected, result);
-//    }
+
+	@Test
+	public void testFailGetContacts() {
+		ArrayList<Contact> list = new ArrayList();
+		ContactListModel expectedResult = new ContactListModel(list);
+		ContactListModel result = controller.getContacts();
+		assertNotEquals(expectedResult, result);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullGetContacts() {
+		ContactListModel expectedResult = new ContactListModel(null);
+		ContactListModel result = controller.getContacts();
+		assertTrue(false);
+	}
+
+	/**
+	 * Test of updateEvents method, of class AgendaController.
+	 */
+	@Test
+	public void testGet0Events() {
+		ContactListModel list = controller.getContacts();
+		Iterable result = new ArrayList();
+		Iterable expected = PersistenceContext.repositories().events().
+			eventsContactPerDay(contact1, Calendar.getInstance());
+		assertEquals(expected, result);
+	}
+
+	/**
+	 * Test of updateEvents method, of class AgendaController.
+	 */
+	@Test
+	public void testGet1Events() {
+		Calendar date = Calendar.getInstance();
+		date.add(Calendar.YEAR, 3);
+		ContactListModel list = controller.getContacts();
+		Iterable expected = new ArrayList();
+		((ArrayList) expected).add(event3);
+		Iterable result = PersistenceContext.repositories().events().
+			eventsContactPerDay(contact2, date);
+		assertEquals(expected, result);
+	}
+
+	/**
+	 * Test of updateEvents method, of class AgendaController.
+	 */
+	@Test
+	public void testGet2Events() {
+		Calendar date = Calendar.getInstance();
+		date.add(Calendar.YEAR, 5);
+		ContactListModel list = controller.getContacts();
+		Iterable expected = new ArrayList();
+		((ArrayList) expected).add(event1);
+		((ArrayList) expected).add(event3);
+		Iterable result = PersistenceContext.repositories().events().
+			eventsContactPerDay(contact2, date);
+		assertEquals(expected, result);
+	}
+
+	/**
+	 * Test of updateEvents method, of class AgendaController.
+	 */
+	@Test
+	public void testNextDay() {
+		Calendar expected = Calendar.getInstance();
+		expected.add(Calendar.DAY_OF_MONTH, 1);
+		Calendar result = controller.nextDay(Calendar.getInstance());
+		assertEquals(expected, result);
+	}
+
+	/**
+	 * Test of updateEvents method, of class AgendaController.
+	 */
+	@Test
+	public void testPreviousDay() {
+		Calendar expected = Calendar.getInstance();
+		expected.add(Calendar.DAY_OF_MONTH, -1);
+		Calendar result = controller.previousDay(Calendar.getInstance());
+		assertEquals(expected, result);
+	}
+
 }
