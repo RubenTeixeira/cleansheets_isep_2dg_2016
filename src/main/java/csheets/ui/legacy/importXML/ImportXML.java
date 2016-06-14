52c5eaab42863c4321e78679ccda2edf7e9bc60a
Carlos Santos <1140611@isep.ipp.pt>
2016-06-14 11:10:20 +0100

    LPFOURDG-290 #time 1h #comment Start code skeleton of lang08.2

---
+++ src/main/java/csheets/ui/legacy/importXML/ImportXML.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csheets.ui.legacy.importXML;
+
+import csheets.core.Spreadsheet;
+import csheets.core.Workbook;
+import csheets.ui.ctrl.UIController;
+
+/**
+ *
+ * @author scarl
+ */
+public class ImportXML {
+
+	public ImportXML() {
+
+	}
+
+	static public void importWorkbook(String tagWorkbook, String tagSpreadSheet,
+									  String tagRow, String tagColumn,
+									  Workbook workbook) {
+
+	}
+
+	static public void importSpreadsheet(String tagSpreadSheet, String tagRow,
+										 String tagColumn,
+										 Spreadsheet spreadsheet) {
+
+	}
+
+	static public void importSpreadsheetSelected(String tagSpreadSheet,
+												 String tagRow,
+												 String tagColumn,
+												 UIController uiController) {
+
+	}
+}
