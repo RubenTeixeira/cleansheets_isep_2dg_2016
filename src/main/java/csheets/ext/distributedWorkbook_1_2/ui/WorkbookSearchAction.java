e0d90e5e6eae50f2d858b73217c70878ef0a0a20
1140780@isep.ipp.pt <1140780@isep.ipp.pt>
2016-06-14 12:11:59 +0100

    LPFOURDG-274 finished analysis #time 8h

---
+++ src/main/java/csheets/ext/distributedWorkbook_1_2/ui/WorkbookSearchAction.java
@@ -0,0 +1,54 @@
+package csheets.ext.distributedWorkbook_1_2.ui;
+
+import csheets.ext.distributedWorkbook.ui.*;
+import csheets.ui.ctrl.BaseAction;
+import csheets.ui.ctrl.UIController;
+import java.awt.event.ActionEvent;
+
+public class WorkbookSearchAction extends BaseAction {
+
+	/**
+	 * The user interface controller
+	 */
+	protected UIController uiController;
+
+	/**
+	 * The distributed workbook search controller.
+	 */
+	private final DistributedWorkbookSearchController distributedSearchController;
+
+	/**
+	 * Creates a new action.
+	 *
+	 * @param uiController the user interface controller
+	 * @param distributedSearchController Controller
+	 */
+	public WorkbookSearchAction(UIController uiController,
+								DistributedWorkbookSearchController distributedSearchController) {
+		this.uiController = uiController;
+		this.distributedSearchController = distributedSearchController;
+	}
+
+	@Override
+	protected String getName() {
+		return "Instance Workbook Search";
+	}
+
+	@Override
+	protected void defineProperties() {
+	}
+
+	/**
+	 * A simple action that presents a confirmation dialog. If the user confirms
+	 * then the contents of the cell A1 of the current sheet are set to the
+	 * string "Changed".
+	 *
+	 * @param event the event that was fired
+	 */
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		WorkbookSearchUI distributedSearch = new WorkbookSearchUI(uiController, distributedSearchController);
+		distributedSearch.run();
+	}
+
+}
