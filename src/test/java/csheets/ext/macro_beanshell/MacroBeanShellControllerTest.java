112648ff09d9893d8612621c2227f7de95ac6290
Jose_Barros <1140364@isep.ipp.pt>
2016-06-15 11:59:03 +0100

    LPFOURDG-227 #30m Tests. Worklog updated.

---
+++ src/test/java/csheets/ext/macro_beanshell/MacroBeanShellControllerTest.java
@@ -0,0 +1,77 @@
+package csheets.ext.macro_beanshell;
+
+import csheets.ui.ctrl.UIController;
+import org.junit.Test;
+
+/**
+ *
+ * @author José Barros
+ */
+public class MacroBeanShellControllerTest {
+
+	private MacroBeanShellController controller;
+
+	public MacroBeanShellControllerTest() {
+		controller = new MacroBeanShellController(UIController.getUIController());
+	}
+
+	/**
+	 * Test of executeCode method, of class MacroBeanShellController.
+	 */
+	@Test
+	public void testExecuteCode() {
+//		String scriptType = BeanShell.NAME;
+//		String code = "Hello world";
+//		String result = code;
+//		assertEquals(result, controller.executeCode(scriptType, code));
+	}
+
+	/**
+	 * Test of createExample method, of class MacroBeanShellController.
+	 */
+	@Test
+	public void testCreateExample() {
+		// Missing
+	}
+
+	/**
+	 * Test of saveScript method, of class MacroBeanShellController.
+	 */
+	@Test
+	public void testSaveScript() {
+		// TODO
+	}
+
+	/**
+	 * Test of getSavedScripts method, of class MacroBeanShellController.
+	 */
+	@Test
+	public void testGetSavedScripts() {
+		// TODO
+	}
+
+	/**
+	 * Test of getScriptContent method, of class MacroBeanShellController.
+	 */
+	@Test
+	public void testGetScriptContent() throws Exception {
+		//TODO
+	}
+
+	/**
+	 * Test of deleteScript method, of class MacroBeanShellController.
+	 */
+	@Test
+	public void testDeleteScript() {
+		//TODO
+	}
+
+	/**
+	 * Test of executeScript method, of class MacroBeanShellController.
+	 */
+	@Test
+	public void testExecuteScript() {
+		//TODO
+	}
+
+}
