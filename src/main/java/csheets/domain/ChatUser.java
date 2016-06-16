fc69c61aa0dd3fefeb6f43afe7a85e0edeae9b08
Carlos Santos <1140611@isep.ipp.pt>
2016-06-16 10:01:19 +0100

    Profile added to chat

--- src/main/java/csheets/domain/ChatUser.java
+++ src/main/java/csheets/domain/ChatUser.java
@@ -22,88 +22,92 @@ import javax.persistence.UniqueConstraint;
  */
 @Entity
 @Table(uniqueConstraints = {
-	@UniqueConstraint(columnNames = {"NICKNAME"})})
+    @UniqueConstraint(columnNames = {"NICKNAME"})})
 public class ChatUser implements Serializable {
 
-	@Id
-	@GeneratedValue
-	private Long id;
+    @Id
+    @GeneratedValue
+    private Long id;
 
-	private String nickname;
+    private String nickname;
 
-	private String name;
+    private String name;
 
-	@Lob
-	private byte[] chatIcon;
+    @Lob
+    private byte[] chatIcon;
 
-	@Transient
-	private boolean state;
+    @Transient
+    private boolean state;
 
-	private ArrayList<ChatMessage> history;
+    private ArrayList<ChatMessage> history;
 
-	protected ChatUser() {
-	}
+    protected ChatUser() {
+    }
 
-	public ChatUser(String nick, byte[] icon) {
-		this(null, nick, icon);
-	}
+    public ChatUser(String nick, byte[] icon) {
+        this(null, nick, icon);
+    }
 
-	public ChatUser(String name, String nick, byte[] icon) {
-		this.nickname = nick;
-		this.chatIcon = icon;
-		this.name = name;
-		this.history = new ArrayList<>();
-		this.state = false;
-	}
+    public ChatUser(String name, String nick, byte[] icon) {
+        this.nickname = nick;
+        this.chatIcon = icon;
+        this.name = name;
+        this.history = new ArrayList<>();
+        this.state = false;
+    }
 
-	public String nickname() {
-		return this.nickname;
-	}
+    public String nickname() {
+        return this.nickname;
+    }
 
-	public byte[] icon() {
-		return this.chatIcon;
-	}
+    public byte[] icon() {
+        return this.chatIcon;
+    }
 
-	public String getNickname() {
-		return nickname;
-	}
+    public String getNickname() {
+        return nickname;
+    }
 
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-	public void newChatMessage(String nickname, String text) {
-		this.history.add(new ChatMessage(nickname, text));
-	}
+    public void newChatMessage(String nickname, String text) {
+        this.history.add(new ChatMessage(nickname, text));
+    }
 
-	public boolean state() {
-		return this.state;
-	}
+    public boolean state() {
+        return this.state;
+    }
 
-	public void changeState(boolean state) {
-		this.state = state;
-	}
+    public void changeState(boolean state) {
+        this.state = state;
+    }
 
-	@Override
-	public int hashCode() {
-		int hash = 7;
-		hash = 29 * hash + Objects.hashCode(this.nickname);
-		return hash;
-	}
+    public String name() {
+        return this.name;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		final ChatUser other = (ChatUser) obj;
-		return Objects.equals(this.nickname, other.nickname);
-	}
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.nickname);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ChatUser other = (ChatUser) obj;
+        return Objects.equals(this.nickname, other.nickname);
+    }
 
 }
