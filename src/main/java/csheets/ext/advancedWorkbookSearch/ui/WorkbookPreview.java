18ea4c880ab347ca1d9d247518a86e2ebaaedeeb
1130383 <1130383@isep.ipp.pt>
2016-06-18 15:30:58 +0100

    Worklog Update.

--- src/main/java/csheets/ext/advancedWorkbookSearch/ui/WorkbookPreview.java
+++ src/main/java/csheets/ext/advancedWorkbookSearch/ui/WorkbookPreview.java
@@ -10,11 +10,10 @@ import csheets.core.Spreadsheet;
 import csheets.core.Workbook;
 
 /**
- * A Preview of one specific Workbook. A Preview has a direct connection to one
- * Workbook and only one. It contains a matrix of cells where the position (0,0)
- * of the preview will be the first content cell of the giving workbook. The
- * size of the preview will be 5*5.
- *
+ * A Preview of one specific Workbook. WorkbookPreview is not a Domain Concept
+ * altought it has a connection to one Workbook. It contains a matrix of cells
+ * where the position (0,0) will be the first content cell of the giving
+ * workbook. The size of the preview will be 5*5 but can be changed.
  *
  * @author Pedro Gomes 1130383@isep.ipp.pt
  */
@@ -69,10 +68,10 @@ public class WorkbookPreview {
 		for (int i = 0; i < this.workbook.getSpreadsheetCount(); i++) {
 			Spreadsheet ss = this.workbook.getSpreadsheet(i);
 			for (int j = 0; j < ss.getColumnCount() + 1; j++) {
-				Cell[] column = ss.getColumn(j); //buggy
-				if (checkColumn(column)) {
+				Cell[] column = ss.getColumn(j);
+				if (checkColumn(column)) { //checks in the specific column if there is a non-empty cell.
 					spreadsheet = ss;
-					break outerloop;
+					break outerloop; //breaks outerloop when first non-empty cell is found.
 				}
 			}
 		}
--- src/main/java/csheets/worklog/n1130383/sprint3/package-info.java
+++ src/main/java/csheets/worklog/n1130383/sprint3/package-info.java
@@ -11,9 +11,11 @@
  * <h2>1. Notes</h2>
  *
  * <p>
- * -In this section you should register important notes regarding your work
- * during the week. For instance, if you spend significant time helping a
- * colleague or if you work in more than a feature.-
+ * During this week the work went well as planned. First days dedicated to
+ * Analysis and Design, then the Test phase and finally the feature
+ * Implementation. While implementing the first Analysis and Design solutions,
+ * new concepts were added and therefore changing the previews phases. Most of
+ * the time were invested in implementing my feature and global discussion.</p>
  *
  * <h2>2. Use Case/Feature: Ipc02.2</h2>
  *
@@ -45,9 +47,9 @@
  * <b>Use Case - Workbook Search:</b> The user indicates a pattern and a
  * directory to search for workbooks that resembles the pattern. The search
  * should be performed accordingly and while Cleansheets application is fully
- * available, therefore it should be perform in the background. The user should
- * one-click over a specific result to see a preview or/and double-click to open
- * the result on Clean Sheets application.</p>
+ * available, therefore it should be performed as a background search. The user
+ * can one-click over a specific Workbook found to see a preview or/and
+ * double-click to open the Workbook on Cleansheets application.</p>
  *
  *
  * <h2>4. Analysis</h2>
@@ -58,7 +60,7 @@
  * <img src="doc-files/ipc02.2_analysis.png" alt="Class Diagram Analysis"></p>
  *
  * The Classes to implement are similar to other Extensions that also use a Side
- * Bar plus Classes that handle this feature algorithms.<ul>
+ * Bar plus Classes that handle these feature required algorithms.<ul>
  * <li><b>AWSExtension:</b> This class contains data regarding the Extension:
  * Name, Version. It also provides access to this UIExtension, required to load
  * the Extension in the main User Interface of the application, and will extend
@@ -68,17 +70,21 @@
  * <li><b>AWSUI:</b> Extends UIExtension as all other Extensions UI. It will
  * provide the Side Bar.</li>
  * <li><b>AWSPanel:</b> This class is the actual Side Bar Panel and will contain
- * the components to interact with the User to perform the implemented features
+ * all components to interact with the User to perform the implemented features
  * – Searching for a Workbook, open a preview or/and open the Workbook to the
- * current workspace. The first draft for the side bar panel:<li>
- * <li><b>AWSController:</b> Class in charge of handling the Search results,
- * open the Preview for a specific Workbook or set the current workspace with an
- * opened Workbook.</li>
- * <li><b>WorkbookPreview:</b> This Class will build a preview panel according
- * the Workbook it receives from AWSController. The preview is a 6*4 rectangular
- * area delimited by an upper left corner and a lower right corner. The first
- * Cell with content will fulfil the position (0; 0) and will serve as a
- * reference for the other cells.</li>
+ * current workspace. This Sidebar, aside from Buttons and Text Fields, has also
+ * a DefaultListModel to contain all searched files and a DefaultTableModel to
+ * contain all data regarding the Workbook Preview.</li>
+ * <li><b>AWSController:</b> Class in charge of handling the Search and
+ * providing the results to AWSPanel. It will also communicate with
+ * WorkbookPreview and retrieve a preview of the given Workbook.</li>
+ * <li><b>WorkbookPreview:</b> This Class builds a preview according the
+ * Workbook received from AWSController. The preview is a 5*5 rectangular area
+ * delimited by an upper left corner and a lower right corner. The first Cell
+ * with content will fulfil the position (0; 0) and will serve as a reference
+ * for the other cells. This class works as a <b>service</b> who builds 5*5
+ * preview for any non-corrupted Workbook. The size of the preview can easily be
+ * changed.</li>
  * </ul>
  *
  * <h3>Analysis of the core problem/functionality:</h3>
@@ -90,36 +96,38 @@
  * workspace. This feature will save time and will enhance the experience with
  * the application.</p>
  *
- *
  * <h2>5. Design</h2>
  *
  * <h3>Functional Tests</h3>
  * <p>
  * Tests can be made for class <code>WorkbookPreview</code> in order to evaluate
- * the previwed cells and if they correspond to the workbook. There can also be
- * tests for the method that searchs for workbooks in the current machine. In
+ * the previwed cells and if it correspond to the given workbook. There can also
+ * be tests for the method that searchs for workbooks in the current machine. In
  * order to do so there must be a testing class for <code>AWSController</code>
  * and <code>WindowPreview.</code></p>
+ * <p>
+ * Both classes were tested for this feature requirements and are fully
+ * functional.</p>
  *
  *
  * <h3>UC Realization</h3>
  *
- * <b>First Class Diagram:</b>
+ * <b>Class Diagram:</b>
  *
  * <p>
  * <img src="doc-files/ipc02.2_class_diagram.png" alt="Class Diagram"></p>
  *
- * <b>First Sequence Diagram:</b>
+ * <b>Sequence Diagram:</b>
  * <p>
  * <img src="doc-files/ipc02.2_sequence_diagram.png" alt="Sequence Diagram"></p>
  *
  *
  * <h3>Design Patterns and Best Practices</h3>
  *
- * -Describe new or existing design patterns used in the issue-
  * <p>
- * -You can also add other artifacts to document the design, for instance,
- * database models or updates to the domain model-
+ * Singleton Pattern implemented by ExtensionManager.</p>
+ * <p>
+ * Low Cowpling - High Cohesion.</p>
  *
  * <h2>6. Implementation</h2>
  *
@@ -127,35 +135,60 @@
  * This feature side bar provides the major interaction with the user and access
  * to its functionalities.The user is able to specify a directory to perform the
  * search and a pattern on how the search is based on. Two <b>Text Labels</b>
- * will be implemented alongside with 3 buttons:<ul>
+ * will be implemented alongside with 2 buttons:<ul>
  * <li><b>Search button</b> - Opens a Directory Dialog. The user chooses a
  * directory on the machine.</li>
  * <li><b>Go button</b> - Starts a background search based on the information
  * provided. At this moment a second thread performs the search while all
  * features are available - asynchronous operation.</li>
- * <li><b>Stop button</b> - Stops the search. Useful for when the user already
- * found the workbook required.</li>
+ * <li>
+ * A Grid (DefaultTableModel) will be implemented on the side bar Extension. It
+ * will be a preview of one of the selected workbooks showing a group of cells.
+ * The list of possible selected workbooks will be displayed below the grid
+ * showing all workbooks found.</li>
+ * <li>
+ * Also a List(DefaultListModel) that will contain all Files found from the
+ * Search</li>
  * </ul>
+ *
  * <p>
- * Both Go and Stop buttons will work with auxiliar synchronization.</p>
+ * <b> Final Version of the Side Bar:</b></p>
  * <p>
- * A Grid will also be implemented on the side bar Extension. It will be a
- * preview of one of the selected workbooks showing a group of cells. The list
- * of possible selected workbooks will be displayed below the grid showing all
- * workbooks found.</p>
+ * <img src="http://i.imgur.com/p3T6ZeR.png" alt="Side-Bar"></p>
+ *
+ * <p>
+ * Top-Right Corner has a Gif that is enabled while the Search is running
+ * (Search algorithm).</p>
+ * <p>
+ * <img src="http://i.imgur.com/nY7q5uw.png" alt="Side-Bar-Search-Indicator"></p>
+ *
+ * <p>
+ * The WorkbookPreview was implemented to set up a Preview for any non-corrupted
+ * Workbook. The Algorithm makes use of Iteration to look for the first
+ * non-empty cell in all Spreadsheets available. The cell is saved and used as a
+ * reference to export the content of a block of cells to create the
+ * preview.</p>
+ * <p>
+ * The AWSController Search Algorithm searchs for Files in a given Directory and
+ * makes use of Recursivity to search on the Sub-Directories of that Directory
+ * and so on. For the pattern system the java.util.regex package was used for
+ * matching the given pattern with regular expressions and to validate them.</p>
  *
  *
  *
  * <h2>7. Integration/Demonstration</h2>
  *
- * -In this section document your contribution and efforts to the integration of
- * your work with the work of the other elements of the team and also your work
- * regarding the demonstration (i.e., tests, updating of scripts, etc.)-
+ * <p>
+ * During this week I focused more on my individual feature while still being
+ * available for anyone who needed. All IPC team had great members and the
+ * Demonstration worked really well.</p>
  *
  * <h2>8. Final Remarks</h2>
  *
- * -In this section present your views regarding alternatives, extra work and
- * future work on the issue.-
+ * <p>
+ * After the Demonstration a crucial update on this feature side bar was made
+ * and commited to fix a malfunction on the Search Button as well as selecting a
+ * directory.
  *
  * <h2>9. Work Log</h2>
  * <p>
@@ -210,17 +243,59 @@
  * <p>
  * 1. nothing.
  *
+ * <p>
+ * <b>Thurday</b>
+ * <p>
+ * Today:
+ * <p>
+ * 1. Demonstration.
+ * <p>
+ * 2. Sprint Review.
+ * <p>
+ * 3. Feature Update.
+ * <p>
+ * Blocking:
+ * <p>
+ * 1. nothing.
+ *
  * <h2>10. Self Assessment</h2>
  *
- * -Insert here your self-assessment of the work during this sprint.-
- *
  * <h3>10.1. Design and Implementation</h3>
+ * <p>
+ * A more viable TDD approach was used and all tests made cover 100% of the code
+ * outside UI.</p>
+ *
  *
  * <p>
- * <b>Evidences:</b>
- * <p>
- * - url of commit: ... - description: this commit is related to the
- * implementation of the design pattern ...-
+ * <b>Evidences:</b><ul>
+ * <li>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/c3a5c9fe39154b624680e6b52c20991b968f1004">Analysis
+ * and Design</a></li>
+ * <li>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/822ca145ecc9799881b43cb1ca06f3bdc179953c">Design
+ * and Class Placement</a></li>
+ * <li>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/d5ceafb0d5d6416bfd0218dac52a3632176abf87">Tests
+ * First Commit</a></li>
+ * <li>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/e7b2e49c07aa46c0e27e2928b0cafb2fba1f07ae">Tests
+ * Second Commit</a></li>
+ * <li>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/559501802962845526a28a6e2114394e84d4bd04">Implementation
+ * First Commit</a></li>
+ * <li>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/a073243f859e439345ec0e32c78fc530b00c99ad">Implementation
+ * Second Commit</a></li>
+ * <li>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/dfe1a235369db494ee593d7821b411e90a6bdb3e">Code
+ * Refactoring</a></li>
+ * <li>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/1fb1e915bb8272f439f7c18e0e1963e74a5d977f">Tests
+ * Update</a></li>
+ * <li>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/0958846ef2a30eb7e3c6145257b38a8a9e7f65e1">Feature
+ * UI Update</a></li>
+ * </ul>
  *
  * <h3>10.2. Teamwork</h3>
  *
