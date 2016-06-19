7ba51785d2977f3e22abfa6c8727cf92624dcae2
1131399 <1131399@isep.ipp.pt>
2016-06-19 21:47:02 +0100

    LPFOURDG-324 #time 10m #comment More convertion method

--- src/main/java/csheets/support/Converter.java
+++ src/main/java/csheets/support/Converter.java
@@ -12,7 +12,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import javax.imageio.ImageIO;
+import org.jfree.xml.util.Base64;
 
 /**
  *
@@ -41,6 +44,29 @@ public final class Converter {
 		baos.close();
 
 		return imageInByte;
+	}
+
+	static public String objectToString(Object object) {
+		try {
+			ByteArrayOutputStream bytesOutput = new ByteArrayOutputStream();
+			ObjectOutputStream objectOutput = new ObjectOutputStream(bytesOutput);
+			objectOutput.writeObject(object);
+			objectOutput.flush();
+			return new String(Base64.encode(bytesOutput.toByteArray()));
+		} catch (Exception e) {
+		}
+		return null;
+	}
+
+	static public Object stringToObject(String string) {
+		try {
+			byte[] bytes = Base64.decode(string.toCharArray());
+			ByteArrayInputStream bytesInput = new ByteArrayInputStream(bytes);
+			ObjectInputStream objectInput = new ObjectInputStream(bytesInput);
+			return objectInput.readObject();
+		} catch (Exception e) {
+		}
+		return null;
 	}
 
 }
