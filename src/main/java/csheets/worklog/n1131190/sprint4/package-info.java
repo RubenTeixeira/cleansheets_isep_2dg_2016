a9f6c9e33c8794dfacbd670a3319934644f03dd4
1131190 <1131190@isep.ipp.pt>
2016-06-19 15:01:17 +0100

    LPFOURDG-351 #time 2h #close Analysis done

---
+++ src/main/java/csheets/worklog/n1131190/sprint4/package-info.java
@@ -0,0 +1,95 @@
+/**
+ * Technical documentation regarding the work of the team member (1131190) João
+ * Martins during week4.
+ *
+ * <p>
+ * <b>Scrum Master: no</b>
+ *
+ * <p>
+ * <b>Area Leader: no</b>
+ *
+ * <h2>1. Notes</h2>
+ *
+ *
+ *
+ * <h2>2. Use Case/Feature: CRM04.3</h2>
+ *
+ * Issue in Jira:
+ * <a href="http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-86">LPFOURDG-86</a>
+ *
+ * <h2>3. Requirement</h2>
+ * It should be possible to search for notes (text and lists) within an time
+ * interval. The query expression should also allow to search based on the
+ * contents of the notes. It should be possible to user regular expressions to
+ * search the contents of the notes that are within the time interval. The
+ * result should list all the notes that where found. It should be possible to
+ * open a specific note by double clicking on it in the result list. It should
+ * be possible to export the search results to a range in an worksheet.
+ *
+ * <h2>4. Analysis</h2>
+ * The user enters the start date and end date . The system displays all the
+ * notes and system lists. The user selects a note / list and select a range of
+ * cells. The system exports the information note / list to the selected cells.
+ *
+ *
+ * <h2>5. Design</h2>
+ *
+ * <img src="doc-files/crm04_3_design.png" alt="image">
+ *
+ * <h3>5.3. Classes</h3>
+ *
+ * <h2>9. Work Log</h2>
+ *
+ * <b>Sunday</b>
+ * <p>
+ * Analysis and design of the UC
+ * <p>
+ *
+ * <b>Monday</b>
+ * <p>
+ * Implementation and Testing of the UC
+ * <p>
+ * Helping other collegues
+ * <p>
+ *
+ * <b>Tuesday</b>
+ * <p>
+ * Helping other collegues
+ * <p>
+ *
+ * <b>Wednesday</b>
+ * <p>
+ * Helping other collegues
+ * <p>
+ *
+ * <b>Thursday</b>
+ * <p>
+ * Presentation to the client
+ *
+ *
+ * <h2>10. Self Assessment</h2>
+ *
+ * -
+ *
+ * <h3>10.1. Design and Implementation:3</h3>
+ *
+ *
+ * <b>Evidences:</b>
+ *
+ *
+ * <h3>10.2. Teamwork: ...</h3>
+ *
+ * <h3>10.3. Technical Documentation: ...</h3>
+ *
+ * @author João Martins
+ */
+package csheets.worklog.n1131190.sprint4;
+
+/**
+ * This class is only here so that javadoc includes the documentation about this
+ * EMPTY package! Do not remove this class!
+ *
+ * @author João Martins
+ */
+class _Dummy_ {
+}
