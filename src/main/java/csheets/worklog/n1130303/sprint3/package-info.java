b2eac4d410fb767134f234a010ec792052e59fc4
1130303 <1130303@isep.ipp.pt>
2016-06-21 01:10:26 +0100

    LPFOURDG-64 #comment Updated worklog of sprint3

--- src/main/java/csheets/worklog/n1130303/sprint3/package-info.java
+++ src/main/java/csheets/worklog/n1130303/sprint3/package-info.java
@@ -34,120 +34,111 @@
  *
  *
  * <h2>4. Analysis</h2>
- * From sprint2 that the chat system is done (UC: IPC05.1). The hardest part to
- * implement in this UC is how to check if a chat user is online or offline. UDP
- * is going to be used to find new chat users and to check if already known
- * users are offline or online. TCP is going to be only used to send and receive
- * messages. Chat profile and chat history is going to be persisted using the
- * already implemented persistence framework.
+ * The user selects "Send Message" option in the "Chat Application" menu. The
+ * system prompts the user to insert a nickname and photo. The user inserts a
+ * nickname and selects a photo. The system saves the information and gets the
+ * other available instances in the local network, and presents them in a chat
+ * window. The user selects which instance to send the message. The system gets
+ * the message, and sends them to the targeted instance. The system notifies the
+ * other user with a popup with 5 seconds time. The system put the received/sent
+ * message in a tree control (sidebar). The user closes the chat. The system
+ * notifies all the other instances that it is offline.
  *
+ * <h3>First "analysis" sequence diagram</h3>
+ * The following diagram depicts a proposal for the realization of the
+ * previously described use case. We call this diagram an "analysis" use case
+ * realization because it functions like a draft that we can do during analysis
+ * or early design in order to get a previous approach to the design. For that
+ * reason we mark the elements of the diagram with the stereotype "analysis"
+ * that states that the element is not a design element and, therefore, does not
+ * exists as such in the code of the application (at least at the moment that
+ * this diagram was created).
+ * <h4>Send Message proposal analysis</h4>
+ * <p>
+ * <img src="doc-files/chat_app_analysis_send.png" alt="image">
+ *
+ * <h4>Receive Message proposal analysis</h4>
+ * <p>
+ * <img src="doc-files/chat_app_analysis_receive.png" alt="image">
+ * <p>
+ *
+ * From the previous diagram we see that we need to add a thread for each
+ * conversation. Using a thread manager we can disable a thread when it´s
+ * necessary.
+ *
+ * <h3>Analysis of Core Technical Problem</h3>
+ * The core of communication is expected to communicate in udp and tcp protocols
+ * connections.
+ * <p>
+ * <img src="doc-files/ipc05.2_chat_analysis.png" alt="image">
  *
  * <h2>5. Design</h2>
  *
  * <h3>5.1. Functional Tests</h3>
- * Basically, from requirements and also analysis, we see that the core
- * functionality of this use case is to be able to add an attribute to cells to
- * be used to store a comment/text. We need to be able to set and get its value.
- * Following this approach we can start by coding a unit test that uses a
- * subclass of <code>CellExtension</code> with a new attribute for user comments
- * with the corresponding method accessors (set and get). A simple test can be
- * to set this attribute with a simple string and to verify if the get method
- * returns the same string. As usual, in a test driven development approach
- * tests normally fail in the beginning. The idea is that the tests will pass in
- * the end.
- * <p>
- * see: <code>csheets.ext.comments.CommentableCellTest</code>
+ * Since this feature uses the network framework already implemented any tests
+ * needed to be done are done on that level. For the realization of this use
+ * case I only use/call this framework methods.
  *
  * <h3>5.2. UC Realization</h3>
  * To realize this user story we will need to create a subclass of Extension. We
  * will also need to create a subclass of UIExtension. For the sidebar we need
  * to implement a JPanel. In the code of the extension
- * <code>csheets.ext.style</code> we can find examples that illustrate how to
+ * <code>csheets.ext.chatApp</code> we can find examples that illustrate how to
  * implement these technical requirements. The following diagrams illustrate
  * core aspects of the design of the solution for this use case.
- * <p>
- * <b>Note:</b> It is very important that in the final version of this technical
- * documentation the elements depicted in these design diagrams exist in the
- * code!
  *
- * <h3>Extension Setup</h3>
- * The following diagram shows the setup of the "comments" extension when
- * cleansheets is run.
+ * <h3>Chat send Message</h3>
+ * The following diagram shows the setup of the local connection when
+ * cleansheets's user select share.
  * <p>
- * <img src="doc-files/core02_01_design.png" alt="image">
+ * <img src="doc-files/ipc05.2_udp_design.png" alt="image">
  *
  *
- * <h3>User Selects a Cell</h3>
- * The following diagram illustrates what happens when the user selects a cell.
- * The idea is that when this happens the extension must display in the sidebar
- * the comment of that cell (if it exists).
+ * <h3>Receive Message</h3>
+ * The following diagram illustrates what happens when a instance of cleansheet
+ * receive message.
  * <p>
- * <img src="doc-files/core02_01_design2.png" alt="image">
- *
- * <h3>User Updates the Comment of a Cell</h3>
- * The following diagram illustrates what happens when the user updates the text
- * of the comment of the current cell. To be noticed that this diagram does not
- * depict the actual selection of a cell (that is illustrated in the previous
- * diagram).
- * <p>
- * <img src="doc-files/core02_01_design3.png" alt="image">
+ * <img src="doc-files/ipc05.2_received_design.png" alt="image">
  *
  * <h3>5.3. Classes</h3>
- *
- * -Document the implementation with class diagrams illustrating the new and the
- * modified classes-
+ * <p>
+ * <b>Class Diagram</b>
+ * <p>
+ * Global Class Diagram
+ * <p>
+ * <img src="doc-files/ipc05.2_classdiagram.png" alt="image">
  *
  * <h3>5.4. Design Patterns and Best Practices</h3>
- *
- * -Describe new or existing design patterns used in the issue-
- * <p>
- * -You can also add other artifacts to document the design, for instance,
- * database models or updates to the domain model-
+ * Used "Service" classes instead of having the controller directly controlling
+ * networking related operations.
  *
  * <h2>6. Implementation</h2>
  *
- * -Reference the code elements that where updated or added-
- * <p>
- * -Also refer all other artifacts that are related to the implementation and
- * where used in this issue. As far as possible you should use links to the
- * commits of your work-
  * <p>
  * see:
  * <p>
- * <a href="../../../../csheets/ext/comments/package-summary.html">csheets.ext.comments</a><p>
- * <a href="../../../../csheets/ext/comments/ui/package-summary.html">csheets.ext.comments.ui</a>
+ * <a href="../../../../csheets/ext/chatApp/application/package-summary.html">csheets.ext.chatApp.application</a><p>
+ * <a href="../../../../csheets/ext/chatApp/ui/package-summary.html">csheets.ext.chatApp.ui</a><p>
  *
  * <h2>7. Integration/Demonstration</h2>
- *
- * -In this section document your contribution and efforts to the integration of
- * your work with the work of the other elements of the team and also your work
- * regarding the demonstration (i.e., tests, updating of scripts, etc.)-
+ * Since this feature was the continuation of the IPC05.1 feature I did
+ * communicate with the my colleague that was responsable for that UC. I also
+ * did a lot of pratical tests to ensure that the UC worked in different
+ * scenarios.
  *
  * <h2>8. Final Remarks</h2>
- *
- * -In this section present your views regarding alternatives, extra work and
- * future work on the issue.-
- * <p>
- * As an extra this use case also implements a small cell visual decorator if
- * the cell has a comment. This "feature" is not documented in this page.
- *
+ * As usual, Swing design was the hardest bit on this sprint. Other obstacle
+ * that I had was to make sure that my design didn't fall when the application
+ * is tested with a weak network infrastructure.
  *
  * <h2>9. Work Log</h2>
- *
- * -Insert here a log of you daily work. This is in essence the log of your
- * daily standup meetings.-
- * <p>
- * Example
  * <p>
  * <b>Monday</b>
  * <p>
- * Yesterday I worked on:
- * <p>
- * 1. -nothing-
- * <p>
  * Today
  * <p>
- * 1. Analysis of the...
+ * 1. Analysis of the UC and started asking some explanations to the previous
+ * colleague.
  * <p>
  * Blocking:
  * <p>
@@ -155,47 +146,53 @@
  * <p>
  * <b>Tuesday</b>
  * <p>
- * Yesterday I worked on:
- * <p>
- * 1. ...
- * <p>
  * Today
  * <p>
- * 1. ...
+ * 1. Started to implement my solution. Analysis and Design was made off the
+ * record (old fashion paper).
  * <p>
  * Blocking:
  * <p>
- * 1. ...
+ * 1. Swing design!
+ * <p>
+ * <b>Wednesday</b>
+ * <p>
+ * Today
+ * <p>
+ * 1. Continued to implement my solution. Having a hard time to make the GUI
+ * pretty and functional.
+ * <p>
+ * Blocking:
+ * <p>
+ * 1. Swing design!
+ * <p>
+ * <b>Thursday</b>
+ * <p>
+ * Today
+ * <p>
+ * 1. Finished the implementation in the morning.
+ * <p>
+ * Blocking:
+ * <p>
+ * 1. Windows firewall and poor network infrastructure problems!
  *
  * <h2>10. Self Assessment</h2>
  *
- * -Insert here your self-assessment of the work during this sprint.-
- *
  * <h3>10.1. Design and Implementation:3</h3>
- *
- * 3- bom: os testes cobrem uma parte significativa das funcionalidades (ex:
- * mais de 50%) e apresentam código que para além de não ir contra a arquitetura
- * do cleansheets segue ainda as boas práticas da área técnica (ex:
- * sincronização, padrões de eapli, etc.)
+ * I was a little behind in time because I lost a fair amount understanding how
+ * the network infrastructure works and also how my previous colleague implement
+ * the previous feature. Overall, I think I did a good job: all requirements
+ * were done and the GUI final look was pretty and functional.
  * <p>
  * <b>Evidences:</b>
- * <p>
- * - url of commit: ... - description: this commit is related to the
- * implementation of the design pattern ...-
  *
  * <h3>10.2. Teamwork: ...</h3>
  *
  * <h3>10.3. Technical Documentation: ...</h3>
  *
- * @author alexandrebraganca
+ * @author Rui Freitas <1130303@isep.ipp.pt>
  */
 package csheets.worklog.n1130303.sprint3;
 
-/**
- * This class is only here so that javadoc includes the documentation about this
- * EMPTY package! Do not remove this class!
- *
- * @author alexandrebraganca
- */
 class _Dummy_ {
 }
