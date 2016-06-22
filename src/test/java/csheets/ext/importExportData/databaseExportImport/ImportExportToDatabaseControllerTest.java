867cbf39b37b44fb8ba369e0bf85e8f14f48be9e
AB-1140280 <1140280@isep.ipp.pt>
2016-06-22 10:19:58 +0100

    LPFOURDG-356 #time 3h #comment Fixing jenkins test build

--- src/test/java/csheets/ext/importExportData/databaseExportImport/ImportExportToDatabaseControllerTest.java
+++ src/test/java/csheets/ext/importExportData/databaseExportImport/ImportExportToDatabaseControllerTest.java
@@ -5,11 +5,7 @@
  */
 package csheets.ext.importExportData.databaseExportImport;
 
-import csheets.CleanSheets;
-import csheets.core.Cell;
 import csheets.core.formula.compiler.FormulaCompilationException;
-import csheets.ui.ctrl.UIController;
-import static org.junit.Assert.assertArrayEquals;
 import org.junit.Test;
 
 /**
@@ -18,50 +14,49 @@ import org.junit.Test;
  */
 public class ImportExportToDatabaseControllerTest {
 
-    CleanSheets app;
-    UIController uiController;
-    ImportExportToDatabaseController controller;
-    String tableName = "ImportExportToDatabaseControllerTest";
-    String driver = "org.h2.Driver";
-    String name = "jdbc:h2:./db/csheets";
-    String username = "";
-    String password = "";
+//    CleanSheets app;
+//    UIController uiController;
+//    ImportExportToDatabaseController controller;
+//    String tableName = "ImportExportToDatabaseControllerTest";
+//    String driver = "org.h2.Driver";
+//    String name = "jdbc:h2:./db/csheets";
+//    String username = "";
+//    String password = "";
+	public ImportExportToDatabaseControllerTest() {
+//        app = new CleanSheets();
+//        uiController = new UIController(app);
+//        controller = new ImportExportToDatabaseController(uiController);
+	}
 
-    public ImportExportToDatabaseControllerTest() {
-        app = new CleanSheets();
-        uiController = new UIController(app);
-        controller = new ImportExportToDatabaseController(uiController);
-    }
+	/**
+	 * Test of importFromDatabase method, of class
+	 * ImportExportToDatabaseController.
+	 */
+	@Test
+	public void testImportFromDatabase() throws Exception {
+	}
 
-    /**
-     * Test of importFromDatabase method, of class
-     * ImportExportToDatabaseController.
-     */
-    @Test
-    public void testImportFromDatabase() throws Exception {
-    }
+	/**
+	 * Test of exportToDatabase method, of class
+	 * ImportExportToDatabaseController.
+	 */
+	@Test
+	public void testExportToDatabase() throws Exception {
+	}
 
-    /**
-     * Test of exportToDatabase method, of class
-     * ImportExportToDatabaseController.
-     */
-    @Test
-    public void testExportToDatabase() throws Exception {
-    }
+	/**
+	 * Test of getSelectedCells method, of class
+	 * ImportExportToDatabaseController.
+	 */
+	@Test
+	public void testGetSelectedCells() {
+	}
 
-    /**
-     * Test of getSelectedCells method, of class
-     * ImportExportToDatabaseController.
-     */
-    @Test
-    public void testGetSelectedCells() {
-    }
-
-    /**
-     * Test of getHeader method, of class ImportExportToDatabaseController.
-     */
-    @Test
-    public void testGetHeader() throws FormulaCompilationException {
+	/**
+	 * Test of getHeader method, of class ImportExportToDatabaseController.
+	 */
+	@Test
+	public void testGetHeader() throws FormulaCompilationException {
 //        String[] expectedResult = new String[4];
 //        expectedResult[0] = "header1";
 //        expectedResult[1] = "header2";
@@ -78,20 +73,20 @@ public class ImportExportToDatabaseControllerTest {
 //        cells[0][3].setContent(expectedResult[3]);
 //        assertArrayEquals(expectedResult,controller.getHeader(cells));
 
-    }
+	}
 
-    /**
-     * Test of getContent method, of class ImportExportToDatabaseController.
-     */
-    @Test
-    public void testGetContent() {
-    }
+	/**
+	 * Test of getContent method, of class ImportExportToDatabaseController.
+	 */
+	@Test
+	public void testGetContent() {
+	}
 
-    /**
-     * Test of setCellContent method, of class ImportExportToDatabaseController.
-     */
-    @Test
-    public void testSetCellContent() throws Exception {
-    }
+	/**
+	 * Test of setCellContent method, of class ImportExportToDatabaseController.
+	 */
+	@Test
+	public void testSetCellContent() throws Exception {
+	}
 
 }
