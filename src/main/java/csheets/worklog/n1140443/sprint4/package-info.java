04da02e6543d13227c0f1eb42a994b40785477ad
Diogo Azevedo <1140443@isep.ipp.tp>
2016-06-23 05:40:44 +0100

    LPFOURDG-380  #time 1h #comment worklog update and design #resolve

--- src/main/java/csheets/worklog/n1140443/sprint4/package-info.java
+++ src/main/java/csheets/worklog/n1140443/sprint4/package-info.java
@@ -16,29 +16,39 @@
  *
  * -Notes about the week's work.-
  * <p>
- * -In this section you should register important notes regarding your work
- * during the week. For instance, if you spend significant time helping a
- * colleague or if you work in more than a feature.-
+ * Marcelo Barroso(1130399)- gave me a great help with the udp service class, since i was having problems getting the information from the local area network.
  *
-    * <h2>2. Use Case/Feature: IPC06.3</h2>
+ * <h2>2. Use Case/Feature: IPC06.3</h2>
  *
- * Issue in Jira: http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-31
+ * Issue in Jira: http://jira.dei.isep.ipp.pt:8080/browse/LPFOURDG-68
  * <p>
- * LPFOURDG-113 LANG02.2: Global Variables
+ * LPFOURDG-68 IPC 06.3: Network Explorer
  *
  * <h2>3. Requirements</h2>
- * Cleansheets should now have a new sidebar window to display information about all the instances of
+ *Cleansheets should now have a new sidebar window to display information about all the instances of
  *Cleansheets in the same local network. The window should be based on a tree control. The tree should
  *display at the root all instances of Cleansheets. For each instance it should display its resources as
  *branches of the root element corresponding to the instance. The information should include: workbooks
  *and respective worksheets; loaded extensions (if they are active or inactive) and its description and version.
  *This tree should be updated in real time.
+ * 
  * <h2>4. Analysis</h2>
- First we will be inserting the cleansheets and its elements in the tree, by creating a UdpService which gets all the cleansheets instances in de local network, which updates it each 4 seconds(Real Time in a pratical way, which doesnt cause a drop of the app preformance).Once we got tthe cleansheets, we must transfers its elements to its branches(workbooks,worksheets and its active extensions plus its version and its respective description.
- * <h2>5. Design</h2>
-*
- * <h3>5.1. Functional Tests</h3>
+ * 
+ *First we will be inserting the cleansheets and its elements in the tree, by creating a Udp/Tcp Service which gets all the cleansheets instances in de local network.
+ * Once we got tthe cleansheets, we must transfers its elements to its branches(workbooks,worksheets and its active extensions plus its version and its respective description.
  *
+ * <h2>5. Design</h2>
+ * <h4>send networkExplorer analysis</h4>
+ * <p>
+ * <img src="doc-files/doc-files/networkExplorer_send.png.png" alt="image">
+ * </p>
+ *  *<h4>receive networkExplorer analysis</h4>
+ * <p>
+ * <img src="doc-files/doc-files/networkExplorer_receive.png.png" alt="image">
+ * </p>
+ * <h3>5.1. Functional Tests</h3>
+ * I Created and AppInfo object with all the information to be displayed in a tree(workbooks and its spreadsheets, and extensions and their information).
+ * Using such information to test the Tree in the UI. 
  * <h3>5.2. UC Realization</h3>
  *
  * <h3>5.3. Classes</h3>
@@ -55,11 +65,17 @@
  * </p>
  * 
  * <h2>6. Implementation</h2>
+ * <code>csheets.ext.networkExplorer.ui.NetworkExplorerPanel</code>
+ * <code>csheets.ext.networkExplorer.NetworkExplorerController</code>
+ * <code>csheets.ext.networkExplorer.UdpService</code>
+ * <code>csheets.ext.networkExplorer.domain.AppInfo</code>
+ * <code>csheets.ext.networkExplorer.domain.ExtensionInfo</code>
+ * <code>csheets.ext.networkExplorer.domain.SpreadSheetInfo</code>
+ * <code>csheets.ext.networkExplorer.domain.WorkbookInfo</code>
+ * <code>csheets.ext.networkExplorer.NetworkExplorerExtension</code>
  *
- * 
  * <h2>7. Integration/Demonstration</h2>
- * Has we enter the second week of the project, we start to develop features for
- * the contacts.
+ * 
  *
  * <h2>8. Final Remarks</h2>
  *
@@ -90,28 +106,33 @@
  * </p>
  * Today
  * <p>
- * 
+ * 1.SideBar implementation, and controller 
  * </p>
  * Blocking:
  * <p>
  * 1. nothing
  * </p>
+ * <p>
  * <b>Wednesday</b>
+ * </p>
+ * Yesterday I worked on:
  * <p>
- * ...
+ * 1. Implementation of networkExplorerPanel, and controller 
+ * </p>
+ * Today
  * <p>
- * 1. Nothing.
- *
+ * 1. udp, and changes in domain and UI.Finish worklog and chenges in design. Tests implementation
+ * </p>
+ * Blocking:
+ * <p>
+ * 1. nothing
+ * </p>
  * <h2>10. Self Assessment</h2>
  *
  * -Insert here your self-assessment of the work during this sprint.-
  *
- * <h3>10.1. Design and Implementation:3</h3>
- *
- * 3- bom: os testes cobrem uma parte significativa das funcionalidades (ex:
- * mais de 50%) e apresentam código que para além de não ir contra a arquitetura
- * do cleansheets segue ainda as boas práticas da área técnica (ex:
- * sincronização, padrões de eapli, etc.)
+ * <h3>10.1. Design and Implementation:</h3>
+ *  3- good: the implementation follows all requirements.
  * <p>
  * <b>Evidences:</b>
  * </p>
