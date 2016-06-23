0ebcba1ab4b698c4425c4976e64991a39a07d2b7
AB-1140280 <1140280@isep.ipp.pt>
2016-06-23 12:04:17 +0100

    LPFOURDG-62 #time 30m #comment worklog updates

--- src/main/java/csheets/worklog/n1140280/sprint4/package-info.java
+++ src/main/java/csheets/worklog/n1140280/sprint4/package-info.java
@@ -53,8 +53,8 @@
  * in persistence? No, different package
  * </p>
  * <p>
- * What happens when a table already exixts in a database?
- * It should be dropped and a new table should be created
+ * What happens when a table already exixts in a database? It should be dropped
+ * and a new table should be created
  * </p>
  * </p>
  * After the questions were awnsered design and test started
@@ -74,7 +74,8 @@
  * <img src="doc-files/design_IPC_4_3_Import.png" alt="image">
  * </p>
  * <h3>5.1. Primary Unit Tests</h3>
- * Primary -unit testing is to ensure basic functionality of database connections
+ * Primary -unit testing is to ensure basic functionality of database
+ * connections
  * <p>
  * 1. Try create a test database and vefify that it exists
  * </p>
@@ -111,13 +112,14 @@
  * 7. Export to a already existing table and ensure an error is returning</p>
  * </p>
  *
- * Some unit tests are not implemented since the method getSelectedCells is required which can
- * only be called by instation of the full clean sheets instanciation
+ * Some unit tests are not implemented since the method getSelectedCells is
+ * required which can only be called by instation of the full clean sheets
+ * instanciation
  *
  * <h2>6. Implementation</h2>
  * Implemented a Singleton class to deal with database conection, most of the
- * code is based on queries that work with the database.
- * Controller manages interaction between data acess layer and the UI mostly by gettting and
+ * code is based on queries that work with the database. Controller manages
+ * interaction between data acess layer and the UI mostly by gettting and
  * setting the selected cells content
  *
  *
@@ -149,17 +151,16 @@
  * <p>
  * Afternoon: Applied test driven development to implement the DatabaseConnector
  * had some doubts regarding jdbc and took more time than planned reading about
- * it
- * Night: Started conecting UI buttons to DatabaseConnector using controller methods,
- * testing while implementing
+ * it Night: Started conecting UI buttons to DatabaseConnector using controller
+ * methods, testing while implementing
  * </p>
  * <p>
  * Tomorrow: Finish conecting UI buttons to DatabaseConnector
  * <b>Wednsday</b>
  * <p>
- * Morning: Finished conecting UI buttons to DatabaseConnector,functional feature but some refractor and
- * documentation still needed
- * Afteroon: Finishing the UC and helped IPC 7.3 Battleship game
+ * Morning: Finished conecting UI buttons to DatabaseConnector,functional
+ * feature but some refractor and documentation still needed Afteroon: Finishing
+ * the UC and helped IPC 7.3 Battleship game
  * </p>
  * <p>
  * Tomorrow:
@@ -171,12 +172,20 @@
  * <p>
  * </p>
  * <b>Evidences:</b>
- *
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/bb0b51de5e09bdfa75671e7e3d79e60f4ff756e6">Analysis</a>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/37f6084f3319ba11302797a30a1b544b8fefeafb">First
+ * design</a>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/e00be6d57663e802023c5775ae1e27dc2ae722e8">Design
+ * and test development</a>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/8d7923c0aaaab0107d0db595e2e069b882e69bf6">Design
+ * error fixing</a>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/cefb9b430ca42de6bc3ff166b59701f996a63a02">Testing</a>
+ * <a href="https://bitbucket.org/lei-isep/lapr4-2016-2dg/commits/3d05ec424ee3216cb78ea664dfc8e94d89fe68c9">Implementation</a>
  * <p>
  * </p>
  *
  * <h3>10.2. Teamwork: ...</h3>
- *
+ * Helped on design and implementation options of the IPC 7.3 Battleships
  *
  * <h3>10.3. Technical Documentation: ...</h3>
  *
